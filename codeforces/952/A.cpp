3511186a0bf65649cfb24c834a82f2b897043916
Kshitij Goel <kshitijg9899@gmail.com>
2019-04-04 21:23:00 +0530

    Add solution for problem `A - Quirky Quantifiers` Link: https://codeforces.com/contest/952/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/952/submission/52313545

---
+++ codeforces/952/A.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstring>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<algorithm>
+#include<unordered_set>
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define ll long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
+int main() {
+    fast;
+    int n;
+    cin >> n;
+    if(n % 2 == 1) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+}
