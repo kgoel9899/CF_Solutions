91ab79ef6e1ddcb4a7c6b4e794c1f8fc818205b4
Kshitij Goel <kshitijg9899@gmail.com>
2019-06-28 13:53:00 +0530

    Add solution for problem `A - Vus the Cossack and a Contest` Link: https://codeforces.com/contest/1186/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1186/submission/56195984

---
+++ codeforces/1186/A.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<iomanip>
+#include<cstring>
+#include<vector>
+#include<set>
+#include<map>
+#include<queue>
+#include<stack>
+#include<cmath>
+#include<algorithm>
+#include<unordered_set>
+#include<bits/stdc++.h>
+#define MOD 1000000007
+#define ll long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAX 200010
+using namespace std;
+int main() {
+    fast;
+    int n, m, k;
+    cin >> n >> m >> k;
+    if(m < n || k < n) {
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
+}
