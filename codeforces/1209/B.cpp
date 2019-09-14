293420d5fdcc837c079a11023f869547796a402a
Kshitij Goel <kshitijg9899@gmail.com>
2019-09-14 22:34:00 +0530

    Add solution for problem `B - Koala and Lights` Link: https://codeforces.com/contest/1209/problem/B Tags: implementation, math, number theory, *1300 Ref: https://codeforces.com/contest/1209/submission/60577268

---
+++ codeforces/1209/B.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#define ll long long
+#define MAX 8200
+#define MOD 1000000007
+using namespace std;
+ 
+struct node {
+    int a;
+    int b;
+    char ch;
+};
+ 
+bool compare(node x, node y) {
+    return x.b < y.b;
+}
+ 
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    node arr[n];
+    int ct = 0;
+    for(int i=0;i<n;i++) {
+        cin >> arr[i].a >> arr[i].b;
+        arr[i].ch = s[i];
+        if(s[i] == '1') {
+            ct++;
+        }
+    }
+    int ans = -1;
+    ans = max(ans, ct);
+    sort(arr, arr + n, compare);
+    int check[n];
+    for(int i=0;i<n;i++) {
+        check[i] = -1;
+    }
+    for(int i=1;i<130;i++) {
+        int temp = ct;
+        for(int j=0;j<n;j++) {
+            if(check[j] == -1) {
+                if(arr[j].b <= i) {
+                    check[j] = 1;
+                    if(arr[j].ch == '1') {
+                        arr[j].ch = '0';
+                        temp--;
+                    } else {
+                        arr[j].ch = '1';
+                        temp++;
+                    }
+                }
+            } else {
+                if((i - arr[j].b) % arr[j].a == 0) {
+                    if(arr[j].ch == '1') {
+                        arr[j].ch = '0';
+                        temp--;
+                    } else {
+                        arr[j].ch = '1';
+                        temp++;
+                    }
+                }
+            }
+        }
+        ct = temp;
+        ans = max(ans, temp);
+    }
+    cout << ans << endl;
+}
+ 
