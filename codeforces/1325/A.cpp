2239e3984f572dab54ccc015d0424586fa16ad89
Kshitij Goel <kshitijg9899@gmail.com>
2020-04-07 14:22:00 +0530

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/75726837

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff                   first
+#define ss                   second
+#define ll                   long long
+#define ld                   double
+#define pb                   push_back
+#define mp                   make_pair
+#define MAX                  10000
+#define mod                  998244353
+#define inf                  1e18
+#define w(x)                 int t; cin >> t; while(t--)
+#define fori(i, a, b)        for(int i = a; i <= b; i++)
+#define ford(i, b, a)        for(int i = b; i >= a; i--)
+#define mk(arr,n,type)       type* arr = new type[n];
+#define p(type1, type2)      pair<type1, type2>
+#define v(type)              vector<type>
+#define m(type1, type2)      map<type1,type2>
+#define um(type1, type2)     unordered_map<type1, type2>
+#define setbits(x)           __builtin_popcountll(x)
+#define fast                 ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fast;
+
+	w(t) {
+		ll x;
+		cin >> x;
+		cout << 1 << " " << x - 1 << endl;
+	}
+
+	return 0;
+}
