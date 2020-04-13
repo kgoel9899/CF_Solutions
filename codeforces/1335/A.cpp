db817d9614e7ce8038eb46ca342634ee73896b90
Kshitij Goel <kshitijg9899@gmail.com>
2020-04-13 22:09:00 +0530

    Add solution for problem `A - Candies and Two Sisters` Link: https://codeforces.com/contest/1335/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1335/submission/76612203

---
+++ codeforces/1335/A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff                   first
+#define ss                   second
+#define ll                   long long
+#define ld                   double
+#define pb                   push_back
+#define mp                   make_pair
+#define MAX                  100001
+#define mod                  998244353
+#define inf                  1e18
+#define w(x)                 int t; cin >> t; while(t--)
+#define fori(i, a, b)        for(int i = a; i <= b; i++)
+#define ford(i, b, a)        for(int i = b; i >= a; i--)
+#define mk(arr,n,type)       type* arr = new type[n];
+#define pii                  pair<int, int>
+#define vi                   vector<int>
+#define um                   unordered_map<int, int>
+#define setbits(x)           __builtin_popcountll(x)
+#define fast                 ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define out(flag)            if(flag == 1) cout << "NO" << endl; else cout << "YES" << endl;
+
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fast;
+
+	ll t;
+	cin >> t;
+	while (t--) {
+		ll n;
+		cin >> n;
+		if (n <= 2) cout << 0 << endl;
+		else cout << (n - 1) / 2 << endl;
+	}
+
+	return 0;
+}
