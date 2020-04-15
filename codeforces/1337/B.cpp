6cc56dad40fbe85baa97fa8f21ba0a6faf5ecb2e
Kshitij Goel <kshitijg9899@gmail.com>
2020-04-16 03:01:00 +0530

    Add solution for problem `B - Kana and Dragon Quest game` Link: https://codeforces.com/contest/1337/problem/B Tags: greedy, implementation, math, *900 Ref: https://codeforces.com/contest/1337/submission/76908707

---
+++ codeforces/1337/B.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define w(x) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i <= b; i++)
+#define repd(i, b, a) for(int i = b; i >= a; i--)
+#define mk(arr,n,type) type* arr = new type[n];
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define um unordered_map
+#define us unordered_set
+#define pqm priority_queue<int>
+#define pqmi priority_queue<int, vi, greater<int>>
+#define lb(v, val) lower_bound(v.begin(), v.end(), val) - v.begin();
+#define ub(v, val) upper_bound(v.begin(), v.end(), val) - v.begin();
+#define setbits(x) __builtin_popcountll(x)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define out(flag) flag ? cout << "NO" << endl : cout << "YES" << endl;
+#define MAX 100001
+#define mod 998244353
+#define inf 1e18
+#define PI 3.1415926535
+
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	fast;
+}
+
+void solve() {
+	int x, n, m;
+	cin >> x >> n >> m;
+	if (x <= 10 * m) {
+		cout << "YES" << endl;
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		x /= 2;
+		x += 10;
+		if (x <= 10 * m) {
+			cout << "YES" << endl;
+			return;
+		}
+	}
+	x -= 10 * m;
+	if (x <= 0) {
+		cout << "YES" << endl;
+		return;
+	}
+	cout << "NO" << endl;
+	return;
+}
+
+int main() {
+
+	IO();
+
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+
+	return 0;
+}
