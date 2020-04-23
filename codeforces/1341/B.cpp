d6ff66cf055802e198a006871b2bacc6d8d50bbe
Kshitij Goel <kshitijg9899@gmail.com>
2020-04-23 20:53:00 +0530

    Add solution for problem `B - Nastya and Door` Link: https://codeforces.com/contest/1341/problem/B Tags: greedy, implementation, *1300 Ref: https://codeforces.com/contest/1341/submission/77801596

--- codeforces/1341/B.cpp
+++ codeforces/1341/B.cpp
@@ -1,54 +1,46 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-int32_t main() {
-    fast;
-    int t = 1;
-    cin >> t;
-    while(t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-        }
-        int curr = 0, mx = INT_MIN, ans = INT_MAX;
-        set<int> s;
-        for(int i=0;i<=n;i++) {
-            int add = 0;
-            if(i >= k) {
-                // for(auto j : s) {
-                //     cout << j << " ";
-                // }
-                // cout << endl;
-                if(s.count(i - k) != 0) {
-                    curr--;
-                    add++;
-                }
-                if(s.count(i - 1) != 0) {
-                    curr--;
-                    add++;
-                }
-                if(curr > mx) {
-                    mx = curr;
-                    ans = i - k;
-                }
-                // cout << curr << " " << ans << endl;
-                // cout << endl;
-                curr += add;
-                add = 0;
-                if(s.count(i - k) != 0) curr--;
-            }
-            if(i < n - 1 && i > 0 && v[i] > v[i + 1] && v[i] > v[i - 1]) {
-                s.insert(i);
-                curr++;
-            }
-        }
-        cout << mx + 1 << " " << ans + 1 << endl;
-    }
+
+int main() {
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		vector<int> v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		map<int, vector<int>> m;
+		vector<int> vis(n, 0);
+		int maxi = INT_MIN;
+		int ct = 0;
+		for (int i = 1; i < k - 1; i++) {
+			if (v[i] > v[i - 1] && v[i] > v[i + 1]) {
+				ct++;
+				vis[i] = 1;
+			}
+		}
+		maxi = max(maxi, ct);
+		if (m[ct].size() == 0) {
+			m[ct].push_back(1);
+		}
+		int curr = k - 1;
+		for (int i = 2; curr < n - 1; i++) {
+			if (vis[i - 1] == 1) {
+				ct--;
+			}
+			if (v[curr] > v[curr - 1] && v[curr] > v[curr + 1]) {
+				vis[curr] = 1;
+				ct++;
+			}
+			if (m[ct].size() == 0) m[ct].push_back(i);
+			maxi = max(maxi, ct);
+			curr++;
+		}
+		cout << maxi + 1 << " " << m[maxi][0] << endl;
+	}
+
+	return 0;
 }
