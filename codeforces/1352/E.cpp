ee087cc4f8605d524b83f0ff091faf3c286e76b5
Kshitij Goel <kshitijg9899@gmail.com>
2020-05-10 12:48:00 +0530

    Add solution for problem `E - Special Elements` Link: https://codeforces.com/contest/1352/problem/E Tags: brute force, implementation, two pointers, *1500 Ref: https://codeforces.com/contest/1352/submission/79624956

---
+++ codeforces/1352/E.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define w(x) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i <= b; i++)
+#define repd(i, b, a) for(int i = b; i >= a; i--)
+#define mk(arr,n,type) type* arr = new type[n];
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define um unordered_map
+#define us unordered_set
+#define pqm priority_queue<int>
+#define pqmi priority_queue<int, vi, greater<int>>
+#define lb(v, val) lower_bound(v.begin(), v.end(), val) - v.begin();
+#define ub(v, val) upper_bound(v.begin(), v.end(), val) - v.begin();
+#define setbits(x) __builtin_popcountll(x)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define out(flag) flag ? cout << "NO" << endl : cout << "YES" << endl;
+#define MAX 100001
+#define mod 998244353
+#define inf 1e18
+#define PI 3.1415926535
+
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+
+int main() {
+
+	IO();
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		int ct = 0;
+		for (int i = 0; i < n; i++) {
+			int summ = 0;
+			int l = -1;
+			for (int j = 0; j < n; j++) {
+				summ += v[j];
+				if (summ == v[i] && j - l >= 2) {
+					ct++;
+					break;
+				} else {
+					while (summ > v[i]) {
+						l++;
+						summ -= v[l];
+					}
+					if (summ == v[i] && j - l >= 2) {
+						ct++;
+						break;
+					}
+				}
+			}
+		}
+		cout << ct << endl;
+	}
+
+	return 0;
+}
