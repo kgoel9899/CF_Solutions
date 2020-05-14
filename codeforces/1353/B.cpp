9f90a5749abf99355c6ba94b9697bbfbe1d13709
Kshitij Goel <kshitijg9899@gmail.com>
2020-05-14 20:15:00 +0530

    Add solution for problem `B - Two Arrays And Swaps` Link: https://codeforces.com/contest/1353/problem/B Tags: greedy, sortings, *800 Ref: https://codeforces.com/contest/1353/submission/80088896

---
+++ codeforces/1353/B.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define w(x) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i <= b; i++)
+#define repd(i, b, a) for(int i = b; i >= a; i--)
+#define mk(arr,n,type) type* arr = new type[n];
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define um unordered_map
+#define us unordered_set
+#define pqm priority_queue<int>
+#define pqmi priority_queue<int, vi, greater<int>>
+#define lb(v, val) lower_bound(v.begin(), v.end(), val) - v.begin();
+#define ub(v, val) upper_bound(v.begin(), v.end(), val) - v.begin();
+#define setbits(x) __builtin_popcountll(x)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define out(flag) flag ? cout << "NO" << endl : cout << "YES" << endl;
+#define MAX 100001
+#define mod 998244353
+#define inf 1e18
+#define PI 3.1415926535
+
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+
+int main() {
+
+	IO();
+
+	int t;
+	cin >> t;
+	while (t--) {
+		int n, k;
+		cin >> n >> k;
+		vector<int> a(n), b(n);
+		ll summ = 0;
+		for (int i = 0; i < n; i++) {
+			cin >> a[i];
+			summ += a[i];
+		}
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+		}
+		sort(a.begin(), a.end());
+		sort(b.begin(), b.end(), greater<int>());
+		int i = 0;
+		while (k && i < n) {
+			if (a[i] <= b[i]) {
+				summ -= a[i];
+				summ += b[i];
+				i++;
+				k--;
+			} else {
+				break;
+			}
+		}
+		cout << summ << endl;
+	}
+
+	return 0;
+}
