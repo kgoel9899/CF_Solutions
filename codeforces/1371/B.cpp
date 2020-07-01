5f040a84fba451bd657faf5411661e2903fb7bbd
Kshitij Goel <kshitijg9899@gmail.com>
2020-07-01 19:53:00 +0530

    Add solution for problem `B - Magical Calendar` Link: https://codeforces.com/contest/1371/problem/B Tags: math, *1200 Ref: https://codeforces.com/contest/1371/submission/85659240

---
+++ codeforces/1371/B.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff first
+#define ss second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define w(x) int t; cin >> t; while(t--)
+#define rep(i, a, b) for(int i = a; i <= b; i++)
+#define repd(i, b, a) for(int i = b; i >= a; i--)
+#define mk(arr,n,type) type* arr = new type[n];
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vl vector<ll>
+#define um unordered_map
+#define us unordered_set
+#define pqm priority_queue<int>
+#define pqmi priority_queue<int, vi, greater<int>>
+#define lb(v, val) lower_bound(v.begin(), v.end(), val) - v.begin();
+#define ub(v, val) upper_bound(v.begin(), v.end(), val) - v.begin();
+#define setbits(x) __builtin_popcountll(x)
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define out(flag) flag ? cout << "NO" << endl : cout << "YES" << endl;
+#define MAX 100001
+#define mod 998244353
+#define inf 1e18
+#define PI 3.1415926535
+
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+
+int main() {
+
+	IO();
+
+	ll t;
+	cin >> t;
+	while (t--) {
+		ll n, r;
+		cin >> n >> r;
+		vector<ll> v;
+		for (ll i = 1; i <= (ll)sqrt(n); i++) {
+			if (n % i == 0) {
+				if (n / i == i) {
+					v.push_back(i);
+				} else {
+					v.push_back(i);
+					v.push_back(n / i);
+				}
+			}
+		}
+		// for (int i = 0; i < v.size(); i++) {
+		// 	cout << v[i] << " ";
+		// }
+		// cout << endl;
+
+		ll temp = min(n - 1, r);
+		ll ans = ((temp) * (temp + 1)) / 2;
+		if (r >= n) ans++;
+		cout << ans << endl;
+	}
+
+	return 0;
+}
