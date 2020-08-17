1de665740256a61b32740228205310cd9920726a
Kshitij Goel <kshitijg9899@gmail.com>
2020-08-17 22:44:00 +0530

    Add solution for problem `A - Omkar and Password` Link: https://codeforces.com/contest/1392/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1392/submission/90251477

---
+++ codeforces/1392/A.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 998244353
+#define ll long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+int main() {
+	IO();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		int i = 0;
+		for (; i < n; i++) {
+			if (v[i] != v[0]) break;
+		}
+		if (i == n) cout << n << endl;
+		else cout << 1 << endl;
+	}
+}
