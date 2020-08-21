ecb3ffd1baa30bad65105fcffab548d43b1fcd1c
Kshitij Goel <kshitijg9899@gmail.com>
2020-08-21 20:56:00 +0530

    Add solution for problem `C - Mere Array` Link: https://codeforces.com/contest/1401/problem/C Tags: constructive algorithms, math, number theory, sortings, *1300 Ref: https://codeforces.com/contest/1401/submission/90581032

--- codeforces/1401/C.cpp
+++ codeforces/1401/C.cpp
@@ -20,22 +20,46 @@ int main() {
 	while (t--) {
 		int n;
 		cin >> n;
-		vector<int> v(n), temp(n);
+		vector<int> v(n), temp;
 		int mini = INT_MAX;
+		map<int, int> m;
+		map<int, vector<int>> tempm;
 		for (int i = 0; i < n; i++) {
 			cin >> v[i];
 			mini = min(mini, v[i]);
-			temp[i] = v[i];
+			m[v[i]]++;
 		}
-		int flag = 0;
-		sort(temp.begin(), temp.end());
 		for (int i = 0; i < n; i++) {
 			if (v[i] % mini != 0) {
-				if (v[i] != temp[i]) {
+				temp.push_back(v[i]);
+				tempm[v[i]].push_back(i);
+			}
+		}
+		int flag = 0;
+		for (int i = 1; i < (int)(temp.size()); i++) {
+			if (temp[i] < temp[i - 1]) {
+				flag = 1;
+				break;
+			}
+		}
+		int curr = 0;
+		for (auto i : m) {
+			vector<int> v = tempm[i.first];
+			if (v.size() != 0) {
+				// cout << i.first << endl;
+				// cout << i.first << " " << curr << endl;
+				if (v[0] != curr) {
 					flag = 1;
 					break;
 				}
+				for (int j = 1; j < (int)(v.size()); j++) {
+					if (v[j] != v[j - 1] + 1) {
+						flag = 1;
+						break;
+					}
+				}
 			}
+			curr += i.second;
 		}
 		if (flag == 1) cout << "NO" << endl;
 		else cout << "YES" << endl;
