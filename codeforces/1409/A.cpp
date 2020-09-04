004b78402ce1c92714854db65c8a93916806aeae
Kshitij Goel <kshitijg9899@gmail.com>
2020-09-04 22:21:00 +0530

    Add solution for problem `A - Yet Another Two Integers Problem` Link: https://codeforces.com/contest/1409/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1409/submission/91878711

---
+++ codeforces/1409/A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 1000000007
+#define ll long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+int main() {
+	IO();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		int ans = abs(a - b) / 10;
+		if (abs(a - b) % 10 != 0) ans++;
+		cout << ans << endl;
+	}
+}
