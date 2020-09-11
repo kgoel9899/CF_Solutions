b32f5daab3aa02a166fc8c96432fc390745557b2
Kshitij Goel <kshitijg9899@gmail.com>
2020-09-11 11:12:00 +0530

    Add solution for problem `B - Bad Prices` Link: https://codeforces.com/contest/1213/problem/B Tags: data structures, implementation, *1100 Ref: https://codeforces.com/contest/1213/submission/92471884

---
+++ codeforces/1213/B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 1000000007
+#define ll long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+void IO() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	fast;
+}
+int main() {
+	IO();
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		int n;
+		cin >> n;
+		vector<int> v(n);
+		for (int i = 0; i < n; i++) {
+			cin >> v[i];
+		}
+		int mn = v[n - 1], ans = 0;
+		for (int i = n - 2; i >= 0; i--) {
+			if (v[i] > mn) ans++;
+			else mn = v[i];
+		}
+		cout << ans << endl;
+	}
+}
