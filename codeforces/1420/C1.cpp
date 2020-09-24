d8e9665cd3390d0d8fa0b463aa1edc5612138429
Kshitij Goel <kshitijg9899@gmail.com>
2020-09-24 20:41:00 +0530

    Add solution for problem `C1 - Pokémon Army (easy version)` Link: https://codeforces.com/contest/1420/problem/C1 Tags: constructive algorithms, dp, greedy, *1300 Ref: https://codeforces.com/contest/1420/submission/93680448

--- codeforces/1420/C1.cpp
+++ codeforces/1420/C1.cpp
@@ -1,68 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define MAX 100001
 #define MOD 1000000007
-#define int long long
+#define ll long long
+#define ld long double
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-const int INF = 1e18;
-const int N = 3e5 + 10;
-vector<int> v(N);
-int ans = 0, n;
-void erase(int i) {
-    if(i == 0 || i == n + 1) return;
-    if(v[i] > v[i - 1] && v[i] > v[i + 1]) ans -= v[i];
-    if(v[i] < v[i - 1] && v[i] < v[i + 1]) ans += v[i];
-}
-void insert(int i) {
-    if(i == 0 || i == n + 1) return;
-    if(v[i] > v[i - 1] && v[i] > v[i + 1]) ans += v[i];
-    if(v[i] < v[i - 1] && v[i] < v[i + 1]) ans -= v[i];
-}
-int32_t main() {
-    fast;
-    int t = 1;
-    cin >> t;
-    while(t--) {
-        int q;
-        cin >> n >> q;
-        for(int i=1;i<=n;i++) {
-            cin >> v[i];
-        }
-        v[0] = -1;
-        v[n + 1] = -1;
-        ans = 0;
-        for(int i=1;i<=n;i++) {
-            if(v[i] > v[i - 1] && v[i] > v[i + 1]) ans += v[i];
-            else if(v[i] < v[i - 1] && v[i] < v[i + 1]) ans -= v[i];
-        }
-        cout << ans << endl;
-        while(q--) {
-            int l, r;
-            cin >> l >> r;
-            erase(l);
-            erase(l - 1);
-            erase(l + 1);
-            if(l != r) {
-                if(l + 2 == r) erase(r);
-                else if(l + 2 < r) {
-                    erase(r);
-                    erase(r - 1);
-                }
-                erase(r + 1);
-            }
-            swap(v[l], v[r]);
-            insert(l);
-            insert(l - 1);
-            insert(l + 1);
-            if(l != r) {
-                if(l + 2 == r) insert(r);
-                else if(l + 2 < r) {
-                    insert(r);
-                    insert(r - 1);
-                }
-                insert(r + 1);
-            }
-            cout << ans << endl;
-        }
-    }
+int main() {
+	fast;
+	ll t = 1;
+	cin >> t;
+	while(t--) {
+		ll n, q;
+		cin >> n >> q;
+		vector<ll> v(n);
+		for(ll i=0;i<n;i++) {
+			cin >> v[i];
+		}
+		vector<ll> a(n);
+		a[0] = 1;
+		for(ll i=1;i<n;i++) {
+			if(v[i] >= v[i - 1]) a[i] = 1;
+			else a[i] = -1;
+		}
+		// for(int i=0;i<n;i++) {
+		// 	cout << a[i] << " ";
+		// }
+		// cout << endl;
+		ll curr = 1;
+		ll ans = 0;
+		ll mx = 1;
+		for(ll i=0;i<=n;i++) {
+			if((i == n && curr == 1) || (i != n && a[i] != curr)) {
+				// cout << curr << " " << v[i - 1] << endl;
+				ans += curr * v[i - 1];
+				curr *= -1;
+			}
+			mx = max(mx, v[i]);
+		}
+		if(ans == 0) cout << mx << endl;
+		else cout << ans << endl;
+	}
 }
