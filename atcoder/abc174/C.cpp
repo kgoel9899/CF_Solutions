02625ab6d6766214323e249c8410a048e15d48a8
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-07 21:01:00 +0530

    Add solution for problem `C - Repsept` Link: https://atcoder.jp/contests/abc174/tasks/abc174_c Tags: AtCoder, *300 Ref: https://atcoder.jp/contests/abc174/submissions/17243267

---
+++ atcoder/abc174/C.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 1000000007
+#define int long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+int32_t main() {
+	fast;
+	int t = 1;
+	// cin >> t;
+	while(t--) {
+		int k;
+		cin >> k;
+		int total = 0;
+		for(int i=1;i<=1e7;i++) {
+			total = (total * 10 + 7) % k;
+			if(total == 0) {
+				cout << i << endl;
+				return 0;
+			}
+		}
+		cout << -1 << endl;
+	}
+}
