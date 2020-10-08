ee256e76f3df1aaf6eb987f8a75cedc5dd1fd4c8
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-09 01:42:00 +0530

    Add solution for problem `A - A?C` Link: https://atcoder.jp/contests/abc166/tasks/abc166_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc166/submissions/17261137

---
+++ atcoder/abc166/A.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mx 1000010
+#define MOD 1000000007
+#define int long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+int32_t main() {
+	fast;
+	int t = 1;
+	// cin >> t;
+	while(t--) {
+		string s;
+		cin >> s;
+		if(s == "ABC") cout << "ARC" << endl;
+		else cout << "ABC" << endl;
+	}
+}
