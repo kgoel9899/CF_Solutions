519b8186a32700849700a409bc9e35b9bebcde6d
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-17 03:37:00 +0530

    Add solution for problem `A - Uniform String` Link: https://codeforces.com/contest/1092/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1092/submission/95696837

---
+++ codeforces/1092/A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 1000000007
+#define int long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+int32_t main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        for(int i=0;i<n;i++) {
+            cout << char('a' + i % k);
+        }
+        cout << endl;
+    }
+}
