7ac18ccf1be59cfe88476c08e5de947b270ae39c
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-20 01:11:00 +0530

    Add solution for problem `D - Little Girl and Maximum XOR` Link: https://codeforces.com/contest/276/problem/D Tags: bitmasks, dp, greedy, implementation, math, *1700 Ref: https://codeforces.com/contest/276/submission/96017304

---
+++ codeforces/276/D.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MAX 100001
+#define MOD 1000000007
+#define int long long
+#define ld long double
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int l, r;
+        cin >> l >> r;
+        int x = log2(r);
+        int n = pow(2, x);
+        if(l == r) cout << 0 << endl;
+        else if(n > l) cout << (n ^ (n - 1)) << endl;
+        else {
+            while(x > 0) {
+                int f = ((l >> x) & 1), s = ((r >> x) & 1);
+                if(f != s) break;
+                x--;
+            }
+            x++;
+            cout << (int)pow(2, x) - 1 << endl;
+        }
+    }
+}
