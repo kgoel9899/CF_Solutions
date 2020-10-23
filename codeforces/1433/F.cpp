819761a2b1ca12e1654fa89bc19370e2d4717fe5
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-24 01:39:00 +0530

    Add solution for problem `F - Zero Remainder Sum ` Link: https://codeforces.com/contest/1433/problem/F Tags: dp, *2100 Ref: https://codeforces.com/contest/1433/submission/96479613

---
+++ codeforces/1433/F.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+const int N = 71;
+int dp[N][N][N][N], v[N][N];
+int32_t main() {
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int n, m, tot;
+        cin >> n >> m >> tot;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                cin >> v[i][j];
+            }
+        }
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                for(int k=0;k<=m/2;k++) {
+                    for(int l=0;l<tot;l++) {
+                        dp[i][j][k][l] = -INF;
+                    }
+                }
+            }
+        }
+        dp[0][0][0][0] = 0;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                for(int k=0;k<=m/2;k++) {
+                    for(int l=0;l<tot;l++) {
+                        if(j < m - 1) {
+                            dp[i][j + 1][k][l] = max(dp[i][j + 1][k][l], dp[i][j][k][l]);
+                            if(k < m / 2) dp[i][j + 1][k + 1][(l + v[i][j]) % tot] = max(dp[i][j + 1][k + 1][(l + v[i][j]) % tot], dp[i][j][k][l] + v[i][j]);
+                        } else {
+                            dp[i + 1][0][0][l] = max(dp[i + 1][0][0][l], dp[i][j][k][l]);
+                            if(k < m / 2) dp[i + 1][0][0][(l + v[i][j]) % tot] = max(dp[i + 1][0][0][(l + v[i][j]) % tot], dp[i][j][k][l] + v[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        cout << dp[n][0][0][0] << endl;
+    }
+    return 0;
+}
