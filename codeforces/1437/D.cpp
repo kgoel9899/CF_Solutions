99ef074953128710d923c4a4e0742358e923d29f
Kshitij Goel <kshitijg9899@gmail.com>
2020-10-28 01:39:00 +0530

    Add solution for problem `D - Minimal Height Tree` Link: https://codeforces.com/contest/1437/problem/D Tags: graphs, greedy, shortest paths, trees, *1600 Ref: https://codeforces.com/contest/1437/submission/96940414

--- codeforces/1437/D.cpp
+++ codeforces/1437/D.cpp
@@ -1,39 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define MOD 1000000007
-#define int long long
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-int32_t main() {
-    int t = 1;
-    cin >> t;
-    while(t--) {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-        }
-        vector<int> temp;
-        temp.push_back(1);
-        int ans = 1, prev = v[1], curr = 1;
-        for(int i=2;i<n;i++) {
-            if(v[i] > prev) {
-                curr++;
-                prev = v[i];
-            } else {
-                temp[0]--;
-                if(temp[0] == 0) {
-                    temp.erase(temp.begin());
-                    temp.push_back(curr);
-                    curr = 1;
-                    ans++;
-                } else curr++;
-                prev = v[i];
+    #include<bits/stdc++.h>
+    using namespace std;
+    #define MOD 1000000007
+    #define int long long
+    #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    #define endl "\n"
+    const int INF = 1e18;
+    int32_t main() {
+        int t = 1;
+        cin >> t;
+        while(t--) {
+            int n;
+            cin >> n;
+            vector<int> v(n);
+            for(int i=0;i<n;i++) {
+                cin >> v[i];
             }
+            vector<int> temp;
+            temp.push_back(1);
+            int ans = 1, prev = v[1], curr = 1;
+            for(int i=2;i<n;i++) {
+                if(v[i] > prev) {
+                    curr++;
+                    prev = v[i];
+                } else {
+                    int flag = 0;
+                    temp[0]--;
+                    if(temp[0] == 0) {
+                        flag = 1;
+                        temp.erase(temp.begin());
+                        temp.push_back(curr);
+                        curr = 1;
+                    } else curr++;
+                    prev = v[i];
+                    if(flag == 1) ans++;
+                }
+            }
+            cout << ans << endl;
         }
-        cout << ans << endl;
+        return 0;
     }
-    return 0;
-}
