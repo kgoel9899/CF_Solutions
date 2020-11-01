f8143ab17391dcf48960027bf9376bee60bbcaa8
Kshitij Goel <kshitijg9899@gmail.com>
2020-11-01 19:13:00 +0530

    Add solution for problem `C - Division` Link: https://codeforces.com/contest/1445/problem/C Tags: math, number theory, *1500 Ref: https://codeforces.com/contest/1445/submission/97358176

---
+++ codeforces/1445/C.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+const int N = 1e5;
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    vector<int> sieve(N, 1);
+    sieve[0] = 0;
+    sieve[1] = 0;
+    for(int i=2;i*i<=N;i++) {
+        if(sieve[i] == 1) {
+            for(int j=i*i;j<N;j+=i) {
+                sieve[j] = 0;
+            }
+        }
+    }
+    while(t--) {
+        int p, q;
+        cin >> p >> q;
+        if(p < q) cout << p << endl;
+        else if(p == q) {
+            int ans = 1;
+            for(int i=1;i<=sqrt(p)+2;i++) {
+                if(p % i == 0) {
+                    if(i % q != 0) ans = max(ans, i);
+                    int temp = p / i;
+                    if(p % temp == 0) {
+                        if(temp % q != 0) ans = max(ans, temp);
+                    }
+                }
+            }
+            cout << ans << endl;
+        } else {
+            int temp = __gcd(p, q);
+            if(q != temp) cout << p << endl;
+            else {
+                map<int, int> m;
+                int qq = q;
+                for(int i=2;i<=sqrt(qq)+2;i++) {
+                    if(sieve[i] == 1) {
+                        int ct = 0;
+                        while(q % i == 0) {
+                            q /= i;
+                            ct++;
+                        }
+                        if(ct != 0) m[i] = ct;
+                    }
+                }
+                if(q >= N || sieve[q] == 1) m[q] = 1;
+                map<int, int> m1;
+                int pp = p;
+                for(auto i : m) {
+                    int pr = i.first;
+                    int ct = 0;
+                    while(p % pr == 0) {
+                        ct++;
+                        p /= pr;
+                    }
+                    if(ct != 0) m1[pr] = ct;
+                }
+                map<int, int>::iterator it = m.begin(), it1 = m1.begin();
+                int save = 0, ans = 0;
+                while(it != m.end()) {
+                    save = it->first;
+                    ans = max(ans, pp / (int)pow(it->first, it1->second - it->second + 1));
+                    it++;
+                    it1++;
+                }
+                if(save == 0) cout << p << endl;
+                else cout << ans << endl;
+            }
+        }
+    }
+}
