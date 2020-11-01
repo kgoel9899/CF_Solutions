98d4ea1d7e4f4edc5394e95cd53949781a6271ef
Kshitij Goel <kshitijg9899@gmail.com>
2020-11-01 16:59:00 +0530

    Add solution for problem `B - Elimination` Link: https://codeforces.com/contest/1445/problem/B Tags: greedy, math, *900 Ref: https://codeforces.com/contest/1445/submission/97318523

---
+++ codeforces/1445/B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << max(a + b, c + d) << endl;
+    }
+}
