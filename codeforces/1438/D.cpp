cd72ea6fa42869747f888da3adba950f9bc963da
Kshitij Goel <kshitijg9899@gmail.com>
2020-11-14 14:11:00 +0530

    Add solution for problem `D - Powerful Ksenia` Link: https://codeforces.com/contest/1438/problem/D Tags: bitmasks, constructive algorithms, math, *2200 Ref: https://codeforces.com/contest/1438/submission/98364335

---
+++ codeforces/1438/D.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        int xr = 0;
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+            xr ^= v[i];
+        }
+        if(n % 2 == 0 && xr != 0) {
+            cout << "NO" << endl;
+            return 0;
+        }
+        cout << "YES" << endl;
+        vector<vector<int>> ans;
+        for(int i=0;i+2<n;i+=2) {
+            ans.push_back({i + 1, i + 2, i + 3});
+        }
+        if(n % 2 == 0) n--;
+        for(int i=n-3;i-2>=0;i-=2) {
+            ans.push_back({i + 1, i, i - 1});
+        }
+        cout << ans.size() << endl;
+        for(auto i : ans) {
+            cout << i[0] << " " << i[1] << " " << i[2] << endl;
+        }
+    }
+}
