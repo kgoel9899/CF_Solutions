eae81fb2152ada484be3cce00b62750928799a9d
Kshitij Goel <kshitijg9899@gmail.com>
2020-11-18 18:14:00 +0530

    Add solution for problem `B - Sherlock and his girlfriend` Link: https://codeforces.com/contest/776/problem/B Tags: constructive algorithms, number theory, *1200 Ref: https://codeforces.com/contest/776/submission/98810586

---
+++ codeforces/776/B.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+const int mx = 1e5 + 5;
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> sieve(mx, 1);
+        sieve[1] = 0;
+        sieve[0] = 0;
+        for(int i=2;i*i<=mx;i++) {
+            if(sieve[i] == 1) {
+                for(int j=i*i;j<mx;j+=i) {
+                    sieve[j] = max(sieve[j], sieve[i] + 1);
+                }
+            }
+        }
+        set<int> s;
+        for(int i=2;i<=n+1;i++) {
+            s.insert(sieve[i]);
+        }
+        cout << s.size() << endl;
+        for(int i=2;i<=n+1;i++) {
+            cout << sieve[i] << " ";
+        }
+        cout << endl;
+    }
+}
