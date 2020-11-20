3f8fb485a9917dfc3750eab134058afbc192c852
Kshitij Goel <kshitijg9899@gmail.com>
2020-11-20 21:31:00 +0530

    Add solution for problem `B - Soldier and Badges` Link: https://codeforces.com/contest/546/problem/B Tags: brute force, greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/546/submission/99045169

---
+++ codeforces/546/B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        int ans = 0;
+        int last = v[0];
+        for(int i=1;i<n;i++) {
+            if(v[i] > last) last = v[i];
+            else {
+                ans += last - v[i] + 1;
+                v[i] += last - v[i] + 1;
+                last = v[i];
+            }
+        }
+        cout << ans << endl;
+    }
+}
