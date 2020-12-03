698161abe744dd34e2e32aceda5438e95b788552
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-04 02:20:00 +0530

    Add solution for problem `A - Determinant` Link: https://atcoder.jp/contests/abc184/tasks/abc184_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc184/submissions/18550623

---
+++ atcoder/abc184/A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        cout << a * d - b * c << endl;
+    }
+}
