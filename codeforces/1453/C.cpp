7ddf610e781663b21bd012506ca95dbc47b2cbba
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-04 20:23:00 +0530

    Add solution for problem `C - Triangles` Link: https://codeforces.com/contest/1453/problem/C Tags: greedy, implementation, *1700 Ref: https://codeforces.com/contest/1453/submission/100379964

--- codeforces/1453/C.cpp
+++ codeforces/1453/C.cpp
@@ -17,10 +17,16 @@ int32_t main() {
         for(int i=0;i<n;i++) {
             cin >> v[i];
         }
+        vector<vector<int>> dp1(10, vector<int>(n)); //row left
+        vector<vector<int>> dp2(10, vector<int>(n)); //row right
+        vector<vector<int>> dp3(10, vector<int>(n)); //col up
+        vector<vector<int>> dp4(10, vector<int>(n)); //col down
         vector<vector<int>> row(10, vector<int>(2)), col(10, vector<int>(2));
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
                 int num = v[i][j] - '0';
+                if(dp1[num][i] == 0) dp1[num][i] = j + 1;
+                else dp2[num][i] = j + 1;
                 if(row[num][0] == 0) row[num][0] = i + 1;
                 else row[num][1] = i + 1;
             }
@@ -28,11 +34,29 @@ int32_t main() {
         for(int i=0;i<n;i++) { //col
             for(int j=0;j<n;j++) { //row
                 int num = v[j][i] - '0';
+                if(dp3[num][i] == 0) dp3[num][i] = j + 1;
+                else dp4[num][i] = j + 1;
                 if(col[num][0] == 0) col[num][0] = i + 1;
                 else col[num][1] = i + 1;
             }
         }
         vector<int> ans(10, 0);
+        for(int dig=0;dig<10;dig++) {
+            for(int i=0;i<n;i++) { //row
+                if(dp2[dig][i] == 0) continue;
+                int a = (n - 1 - i) * (dp2[dig][i] - dp1[dig][i]);
+                int b = i * (dp2[dig][i] - dp1[dig][i]);
+                ans[dig] = max({ans[dig], a, b});
+            }
+        }
+        for(int dig=0;dig<10;dig++) {
+            for(int i=0;i<n;i++) { //col
+                if(dp4[dig][i] == 0) continue;
+                int a = (n - 1 - i) * (dp4[dig][i] - dp3[dig][i]);
+                int b = i * (dp4[dig][i] - dp3[dig][i]);
+                ans[dig] = max({ans[dig], a, b});
+            }
+        }
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
                 int num = v[i][j] - '0';
