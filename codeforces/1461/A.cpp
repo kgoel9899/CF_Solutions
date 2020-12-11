88459a8339d83e1bc88e7964924820e80cf53b8f
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-12 00:32:00 +0530

    Add solution for problem `A - String Generation` Link: https://codeforces.com/contest/1461/problem/A Tags: constructive algorithms, greedy, *800 Ref: https://codeforces.com/contest/1461/submission/100969507

---
+++ codeforces/1461/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int n, k;
+        cin >> n >> k;
+        for(int i=0;i<n/3;i++) {
+            cout << "abc";
+        }
+        if(n % 3 == 1) cout << "a" << endl;
+        else if(n % 3 == 2) cout << "ab" << endl;
+        else cout << endl;
+    }
+}
