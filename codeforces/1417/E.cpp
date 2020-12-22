5fce2eca0f69f2b03ea798b6a6f1e00160765d46
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-23 02:55:00 +0530

    Add solution for problem `E - XOR Inverse` Link: https://codeforces.com/contest/1417/problem/E Tags: bitmasks, divide and conquer, strings, trees, *2000 Ref: https://codeforces.com/contest/1417/submission/102077001

--- codeforces/1417/E.cpp
+++ codeforces/1417/E.cpp
@@ -1,68 +1,89 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+#pragma GCC optimize ("-O3")
+#define watch(x) cout << (#x) << " = " << (x) << endl
+#define PI double(2 * acos(0.0))
+#define LL long long
 #define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-const int N = 4 * 300010;
-int32_t main() {
-    fast;
-    int t = 1;
-    // cin >> t;
-    while(t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> v(N);
-        int ind = -1;
-        for(int i=0;i<n;i++) {
-            int num;
-            cin >> num;
-            v[ind + 1].push_back(num);
-        }
-        vector<int> ans;
-        int ret = 0, ct = 0;
-        for(int i=31;i>=0;i--) {
-            int temp = ind + 1;
-            ind = ct;
-            int ansone = 0, anszero = 0;
-            for(int j=temp;j<=ind;j++) {
-                vector<int> ones, zeros;
-                int invone = 0, invzero = 0;
-                int sz = v[j].size();
-                for(int k=sz-1;k>=0;k--) {
-                    if(((v[j][k] >> i) & 1) == 1) {
-                        anszero += invzero;
-                        ones.push_back(v[j][k]);
-                        invone++;
-                    } else {
-                        ansone += invone;
-                        zeros.push_back(v[j][k]);
-                        invzero++;
-                    }
-                }
-                if(zeros.size() > 1) {
-                    reverse(zeros.begin(), zeros.end());
-                    v[ct + 1] = zeros;
-                    ct++;
-                }
-                if(ones.size() > 1) {
-                    reverse(ones.begin(), ones.end());
-                    v[ct + 1] = ones;
-                    ct++;
-                }
-            }
-            ret += min(ansone, anszero);
-            if(anszero <= ansone) ans.push_back(0);
-            else ans.push_back(1);
-        }
-        reverse(ans.begin(), ans.end());
-        int num = 0, power2 = 1;
-        for(int i=0;i<ans.size();i++)   {
-            num += ans[i] * power2;
-            power2 *= 2;
-        }
-        cout << ret << " " << num << endl;
+#define all(x) (x).begin(), (x).end()
+#define INF 1e15
+vector<long long> v[8000000];
+
+long long isithBitSet(long long n, long long k) 
+{ 
+    if (n & (1LL << (k))) 
+        return 1; 
+    else
+        return 0; 
+} 
+
+vector<long long> tempOne;
+vector<long long> tempZero;
+
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    long long n;
+    cin >> n;
+    long long x;
+    long long index = -1;
+    vector<long long> finalAns;
+    for(long long i = 0; i < n; i++)    {
+        cin >> x;
+        v[index+1].push_back(x);
     }
+    long long ans = 0;
+    long long count = 0;
+    for(long long i = 31; i >= 0; i--)  {
+        long long temp = index+1;
+        index = count;
+        long long ansOne = 0;
+        long long ansZero = 0;
+        for(long long j = temp; j <= index; j++)    {
+            long long len = v[j].size();
+            long long one = 0;
+            long long zero = 0;
+            tempOne.clear();
+            tempZero.clear();
+            for(long long k = (len-1); k >= 0; k--) {
+                if(isithBitSet(v[j][k], i)) {
+                    ansZero += zero;
+                    one++;
+                    tempOne.push_back(v[j][k]);
+                } else  {
+                    ansOne += one;
+                    zero++;
+                    tempZero.push_back(v[j][k]);
+                }   
+            }
+            if(tempOne.size() > 1)  {
+                v[count+1] = tempOne;
+                reverse(all(v[count+1]));
+                count++;
+            }
+            if(tempZero.size() > 1) {
+                v[count+1] = tempZero;
+                reverse(all(v[count+1]));
+                count++;
+            }
+        }
+        ans += min(ansOne, ansZero);
+        if(ansZero <= ansOne)   {
+            finalAns.push_back(0);
+        } else  {
+            finalAns.push_back(1);
+        }
+    }
+    cout << ans << " ";
+    ans = 0;
+    long long power2 = 1;
+    reverse(all(finalAns));
+    for(long long i = 0; i < (long long)finalAns.size(); i++)   {
+        ans += finalAns[i]*power2;
+        power2 *= 2;
+    }
+    cout << ans;
+    return 0;
 }
+ 
