a8a2b0e027a8cce42c6d200d4fea791b6e661e76
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-24 01:58:00 +0530

    Add solution for problem `E - Little Artem and Time Machine` Link: https://codeforces.com/contest/669/problem/E Tags: data structures, *2000 Ref: https://codeforces.com/contest/669/submission/102150981

---
+++ codeforces/669/E.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+const int N = 1e9 + 5;
+int n;
+map<int, map<int, int>> m;
+void update(int ind, int num, int val) {
+    while(ind < N) {
+        m[num][ind] += val;
+        ind += (ind & (-ind));
+    }
+}
+int query(int ind, int num) {
+    int ans = 0;
+    while(ind > 0) {
+        ans += m[num][ind];
+        ind -= (ind & (-ind));
+    }
+    return ans;
+}
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        cin >> n;
+        while(n--) {
+            int a, t, x;
+            cin >> a >> t >> x;
+            if(a == 1) update(t, x, 1);
+            else if(a == 2) update(t, x, -1);
+            else cout << query(t, x) << endl;
+        }
+    }
+}
