0423e1c2f6fe7ebe029a493a3c75fa5963cb7c7b
Kshitij Goel <kshitijg9899@gmail.com>
2020-12-31 14:21:00 +0530

    Add solution for problem `A - ABC Swap` Link: https://atcoder.jp/contests/abc161/tasks/abc161_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc161/submissions/19079624

---
+++ atcoder/abc161/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    // cin >> t;
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << c << " " << a << " " << b << endl;
+    }
+}
