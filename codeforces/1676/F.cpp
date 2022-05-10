1c00eaf077dc76d734db08242732cb9a2c0619c4
Kshitij Goel <kshitijg9899@gmail.com>
2022-05-10 23:56:00 +0530

    Add solution for problem `F - Longest Strike` Link: https://codeforces.com/contest/1676/problem/F Tags: data structures, greedy, implementation, sortings, two pointers, *1300 Ref: https://codeforces.com/contest/1676/submission/156754135

--- codeforces/1676/F.cpp
+++ codeforces/1676/F.cpp
@@ -1,89 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+ 
+void solve()
+{
+	int n, k;
+	cin >> n >> k;
+	int a[n];
+	map<int, int> mp;
+	for(int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		mp[a[i]]++;
+	}
+	vector<int> c;
+	for(auto x : mp)
+	{
+		if(x.second >= k)
+		{
+			c.push_back(x.first);
+		}
+	}
+	if(c.size() == 0)
+	{
+		cout << -1 << endl;
+		return;
+	}
+	sort(c.begin(), c.end());
+	int mx = 0;
+	int lans = c[0], rans = c[0];
+	int l = c[0];
+	for(int i = 1; i < c.size(); i++)
+	{
+		if(c[i]-1 == c[i-1])
+		{
+			if(c[i]-l > mx)
+			{
+				lans = l;
+				rans = c[i];
+				mx = c[i]-l;
+			}
+		}
+		else
+		{
+			l = c[i];
+		}
+	}
+	cout << lans << " " << rans << endl;
+}
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int t = 1;
-    cin >> t;
-    while(t--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> v(n);
-        map<int, int> m;
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-            m[v[i]]++;
-        }
-        int l = 0, r = -1;
-        vector<int> temp;
-        for(auto i : m) {
-            if(i.second < k) {
-                if(temp.size() != 0) {
-                    int curr = temp[0];
-                    int tl = curr, tr = curr;
-                    // dbg(temp);
-                    for(int j=1;j<temp.size();j++) {
-                        if(temp[j] != curr + 1) {
-                            if(tr - tl > r - l) {
-                                r = tr;
-                                l = tl;
-                            }
-                            tl = temp[j];
-                            tr = temp[j];
-                        } else tr++;
-                        curr = temp[j];
-                    }
-                    if(tr - tl > r - l) {
-                        r = tr;
-                        l = tl;
-                    }
-                }
-                temp.clear();
-            } else {
-                temp.push_back(i.first);
-            }
-        }
-        if(temp.size() != 0) {
-            int curr = temp[0];
-            int tl = curr, tr = curr;
-            // dbg(temp);
-            for(int j=1;j<temp.size();j++) {
-                if(temp[j] != curr + 1) {
-                    if(tr - tl > r - l) {
-                        r = tr;
-                        l = tl;
-                    }
-                    tl = temp[j];
-                    tr = temp[j];
-                } else tr++;
-                curr = temp[j];
-            }
-            if(tr - tl > r - l) {
-                r = tr;
-                l = tl;
-            }
-        }
-        if(l == 0) cout << -1 << endl;
-        else cout << l << " " << r << endl;
-    }
+int main(int argc, char * argv[])
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		solve();
+	}
 }
