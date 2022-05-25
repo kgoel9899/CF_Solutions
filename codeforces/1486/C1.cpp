12131780b3140f76626fefefcf537fee0cc17a47
Kshitij Goel <kshitijg9899@gmail.com>
2022-05-25 23:01:00 +0530

    Add solution for problem `C1 - Guessing the Greatest (easy version)` Link: https://codeforces.com/contest/1486/problem/C1 Tags: binary search, interactive, *1600 Ref: https://codeforces.com/contest/1486/submission/158413546

--- codeforces/1486/C1.cpp
+++ codeforces/1486/C1.cpp
@@ -1,75 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-// #define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+int ask(int l, int r) {
+    if (l >= r) return -1;
+    cout << "? " << l + 1 << ' ' << r + 1 << endl;
+    int ans;
+    cin >> ans;
+    return ans - 1;
+}
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    // cin >> tt;
-    while(tt--) {
-        int n;
-        cin >> n;
-        int beg = 1, end = n;
-        int ans = -1;
-        while(beg <= end) {
-            int mid = (beg + end) / 2;
-            if(beg == end) {
-                ans = beg;
-                break;
-            }
-            cout << "? " << beg << " " << end << endl;
-            if(end - beg == 1) {
-                int q;
-                cin >> q;
-                if(q == beg) ans = end;
-                else ans = beg;
-                break;
-            }
-            int full;
-            cin >> full;
-            if(full >= mid) {
-                cout << "? " << mid << " " << end << endl;
-                int half;
-                cin >> half;
-                if(half == full) {
-                    // max is here
-                    beg = mid;
-                } else {
-                    // max is in other
-                    end = mid - 1;
-                }
+int main() {
+    int n;
+    cin >> n;
+    int l = 0, r = n;
+    while (r - l > 1) {
+        int m = (l + r) / 2;
+        int smax = ask(l, r - 1);
+        if (smax < m) {
+            if (ask(l, m - 1) == smax) {
+                r = m;
             } else {
-                cout << "? " << beg << " " << mid << endl;
-                int half;
-                cin >> half;
-                if(half == full) {
-                    // max is here
-                    end = mid;
-                } else {
-                    // max is in other
-                    beg = mid + 1;
-                }
+                l = m;
+            }
+        } else {
+            if (ask(m, r - 1) == smax) {
+                l = m;
+            } else {
+                r = m;
             }
         }
-        cout << "! " << ans << endl;
     }
+    cout << "! " << r << endl;
+    return 0;
 }
