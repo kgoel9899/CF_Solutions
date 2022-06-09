832e920a8690aa7f099bc4a18896136142a16636
Kshitij Goel <kshitijg9899@gmail.com>
2022-06-09 23:54:00 +0530

    Add solution for problem `C - Wet Shark and Flowers` Link: https://codeforces.com/contest/621/problem/C Tags: combinatorics, math, number theory, probabilities, *1700 Ref: https://codeforces.com/contest/621/submission/160045422

--- codeforces/621/C.cpp
+++ codeforces/621/C.cpp
@@ -1,58 +1,49 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
+#define REP(i, a, b) for (int i = (a), _end_ = (b); i < _end_; ++i)
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+#define mp make_pair
+#define x first
+#define y second
+#define pb push_back
+#define SZ(x) (int((x).size()))
+#define ALL(x) (x).begin(), (x).end()
+
+template<typename T> inline bool chkmin(T &a, const T &b) { return a > b ? a = b, 1 : 0; }
+template<typename T> inline bool chkmax(T &a, const T &b) { return a < b ? a = b, 1 : 0; }
+
+typedef long long LL;
+
+const int oo = 0x3f3f3f3f;
+
+const int Mod = 1e9 + 7;
+
+const int maxn = 100000;
+
+int n, p;
+int l[maxn + 5], r[maxn + 5];
+
+double cnt[maxn + 5], tot[maxn + 5];
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-// editorial
-int32_t main() {
-    fast;
-    int tt = 1;
-    // cin >> tt;
-    while(tt--) {
-        int n, p;
-        cin >> n >> p;
-        vector<vector<int>> v(n, vector<int>(2));
-        for(int i=0;i<n;i++) {
-            cin >> v[i][0] >> v[i][1];
-        }
-        setpres;
-        double s = 0.0;
-        for(int i=0;i<n;i++) {
-            int l = i, r = (i + 1) % n;
-            
-            int tot1 = v[l][1] - v[l][0] + 1;
-            int tot2 = v[r][1] - v[r][0] + 1;
-            
-            int div1 = v[l][1] / p - (v[l][0] - 1) / p;
-            int div2 = v[r][1] / p - (v[r][0] - 1) / p;
-            
-            int not1 = tot1 - div1;
-            int not2 = tot2 - div2;
-            
-            double pr = (not1 / (double) tot1);
-            pr *= (not2 / (double) tot2);
-            pr = 1 - pr;
-            
-            s += pr;
-        }
-        cout << s * 2000 << endl;
-    }
+	scanf("%d%d", &n, &p);
+	REP(i, 0, n) scanf("%d%d", l + i, r + i), ++r[i], cnt[i] = (r[i] - 1) / p - (l[i] - 1) / p, tot[i] = r[i] - l[i];
+	double ans = 0;
+	REP(i, 0, n)
+	{
+		int nxt = (i + 1) % n;
+		double b = tot[i] * tot[nxt];
+		double a = cnt[i] * tot[nxt] + tot[i] * cnt[nxt] - cnt[i] * cnt[nxt];
+		ans += a / b;
+	}
+	ans *= 2000;
+	printf("%.15f\n", ans);
+	return 0;
 }
