08aa01c29833525d244d10d2ba819f127660d699
Kshitij Goel <kshitijg9899@gmail.com>
2022-06-17 23:49:00 +0530

    Add solution for problem `D - Decorate Apple Tree` Link: https://codeforces.com/contest/1056/problem/D Tags: constructive algorithms, dfs and similar, dp, graphs, greedy, sortings, trees, *1600 Ref: https://codeforces.com/contest/1056/submission/160988447

--- codeforces/1056/D.cpp
+++ codeforces/1056/D.cpp
@@ -1,60 +1,90 @@
-#include<bits/stdc++.h>
+#pragma GCC optimize("O3")
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
+using namespace __gnu_pbds; 
+
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define all(v) (v).begin(),(v).end()
+#define f first
+#define s second
+#define sz(x) ll(x.size())
+#define endl '\n'
+#define forn(i,n) for(int i=0;i<n;++i)
+#define fore(i,l,r) for(int i=int(l);i<=int(r);++i)
+#define rep(i,begin,end) for(__typeof(end) i=(begin);i!=(end);i++)
+#define fill(a,value) memset(a,value,sizeof(a));
+#define gcd(a,b) __gcd((a),(b))
+#define watch1(x) cout << (x) << endl
+#define watch2(x,y) cout << (x) << " " << (y) << endl
+#define watch3(x,y,z) cout << (x) << " " << (y) << " " << (z) << endl
+#define fastio ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+ 
+typedef long long ll;
+typedef long double ld; 
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vpii;
+typedef tree<pii,null_type,less<pii>,rb_tree_tag,tree_order_statistics_node_update> oset;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+// find_by_order(k) returns iterator to kth element starting from 0;
+// order_of_key(k) returns count of elements strictly smaller than k;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+const int mod = 998244353;
+const int inf = 9e18;
+const int N = 1e5 + 5;
 
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+vi g[N];
+int ans[N],dp[N];
 
-int n;
-vector<vector<int>> adj;
-vector<int> leaf;
-void dfs(int curr, int par) {
-    int done = 0;
-    for(auto i : adj[curr]) {
-        if(i == par) continue;
-        dfs(i, curr);
-        leaf[curr] += leaf[i];
-        done = 1;
+void dfs(int v,int par){
+    for(int u:g[v]){
+        if(u!=par){
+            dfs(u,v);
+            dp[v]+=dp[u];
+        }
     }
-    if(!done) leaf[curr] = 1;
 }
-int32_t main() {
-    fast;
-    int tt = 1;
-    // cin >> tt;
-    while(tt--) {
-        cin >> n;
-        adj.clear();
-        adj.resize(n + 1);
-        leaf.clear();
-        leaf.resize(n + 1);
-        for(int i=2;i<=n;i++) {
-            int a;
-            cin >> a;
-            adj[i].push_back(a);
-            adj[a].push_back(i);
-        }
-        dfs(1, -1);
-        sort(all(leaf));
-        for(int i=1;i<=n;i++) {
-            cout << leaf[i] << " ";
-        }
-        cout << endl;
+
+void solve(){
+    int i,n;
+    cin>>n;
+    if(n==1){
+        cout<<1;
+        return;
     }
+    for(i=2;i<=n;++i){
+        int p;
+        cin>>p;
+        g[i].pb(p);
+        g[p].pb(i);
+    }
+    for(i=2;i<=n;++i){
+        dp[i]+=(sz(g[i])==1);
+    }
+    dfs(1,0);
+    sort(dp+1,dp+1+n);
+    for(i=1;i<=n;++i){
+        cout<<dp[i]<<" ";
+    }
+}
+
+signed main(){
+    fastio;
+    int t;
+    //cin>>t;
+    t=1;
+    while(t--){
+        solve();
+    }
+    return 0;
 }
