485a2684f3b692f90f36270b28ccf1d3f503e3da
Kshitij Goel <kshitijg9899@gmail.com>
2022-08-06 00:01:00 +0530

    Add solution for problem `D - Color with Occurrences` Link: https://codeforces.com/contest/1714/problem/D Tags: brute force, data structures, dp, greedy, strings, *1600 Ref: https://codeforces.com/contest/1714/submission/167145659

---
+++ codeforces/1714/D.cpp
@@ -0,0 +1,72 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__) 
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        string t;
+        cin >> t;
+        int orig = t.size();
+        int n;
+        cin >> n;
+        vector<string> v(n);
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+        }
+        vector<vector<int>> ans;
+        int l = 0, r = 0;
+        int ok = 1;
+        while(true) {
+            int tl = -1, tr = -1, ind = -1;
+            for(int i=l;i<=r;i++) {
+                for(int j=0;j<n;j++) {
+                    if(i + v[j].size() <= orig && t.substr(i, (int)v[j].size()) == v[j]) {
+                        if(tr < i + (int)v[j].size()) {
+                            tr = i + v[j].size();
+                            tl = i;
+                            ind = j;
+                        }
+                    }
+                }
+            }
+            if(ind == -1) {
+                ok = 0;
+                break;
+            }
+            ans.push_back({ind + 1, tl + 1});
+            r = tr;
+            l = tl + 1;
+            if(tr == orig) break;
+        }
+        if(!ok) {
+            cout << -1 << endl;
+            continue;
+        }
+        cout << ans.size() << endl;
+        for(auto& i : ans) {
+            cout << i[0] << " " << i[1] << endl;
+        }
+    }
+}
