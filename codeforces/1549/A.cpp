e5d3f89ee085366731bf2184db678a02b2de2b59
Kshitij Goel <kshitijg9899@gmail.com>
2022-08-10 23:02:00 +0530

    Add solution for problem `A - Gregor and Cryptography` Link: https://codeforces.com/contest/1549/problem/A Tags: math, number theory, *800 Ref: https://codeforces.com/contest/1549/submission/167785903

---
+++ codeforces/1549/A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+int32_t main() {
+    fast;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        int p;
+        cin >> p;
+        cout << 2 << " ";
+        int rem = p % 2;
+        if(rem == 0) cout << p << endl;
+        else cout << p - 1 << endl;
+    }
+}
