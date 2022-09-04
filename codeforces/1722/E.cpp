81faac7711d0e03bba4b65f4e2367772571341be
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-04 15:07:00 +0530

    Add solution for problem `E - Counting Rectangles` Link: https://codeforces.com/contest/1722/problem/E Tags: brute force, data structures, dp, implementation, *1600 Ref: https://codeforces.com/contest/1722/submission/170822838

---
+++ codeforces/1722/E.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n, q;
+        cin >> n >> q;
+        const int N = 1005;
+        vector<vector<int>> pref(N, vector<int>(N));
+        for(int i=0;i<n;i++) {
+            int h, w;
+            cin >> h >> w;
+            pref[h][w] += h * w;
+        }
+        for(int i=1;i<N;i++) {
+            pref[0][i] += pref[0][i - 1];
+        }
+        for(int i=1;i<N;i++) {
+            pref[i][0] = pref[i - 1][0];
+        }
+        for(int i=1;i<N;i++) {
+            for(int j=1;j<N;j++) {
+                pref[i][j] += pref[i - 1][j] + pref[i][j - 1] - pref[i - 1][j - 1];
+            }
+        }
+        while(q--) {
+            int hs, ws, hb, wb;
+            cin >> hs >> ws >> hb >> wb;
+            cout << pref[hb - 1][wb - 1] - pref[hs][wb - 1] - pref[hb - 1][ws] + pref[hs][ws] << endl;
+        }
+    }
+}
