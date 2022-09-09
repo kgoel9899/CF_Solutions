8a83348ea1c8e2af9dd7379b8d0c9f8495caf2db
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-10 02:45:00 +0530

    Add solution for problem `C - Uncle Bogdan and Country Happiness` Link: https://codeforces.com/contest/1388/problem/C Tags: dfs and similar, greedy, math, trees, *1800 Ref: https://codeforces.com/contest/1388/submission/171547421

--- codeforces/1388/C.cpp
+++ codeforces/1388/C.cpp
@@ -21,21 +21,28 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 
-int n, m, ok;
+int n, m;
 vector<int> p, h, sub, good;
 vector<vector<int>> adj;
-// x = good, y = bad
-// x - y = h[i];
-// x + y = sub[i];
-// x = (h[i] + sub[i]) / 2;
-// y = (sub[i] - h[i]) / 2;
 void dfs(int curr, int par) {
-    int sum = 0;
     sub[curr] += p[curr];
     for(auto i : adj[curr]) {
         if(i == par) continue;
         dfs(i, curr);
         sub[curr] += sub[i];
+    }
+}
+// x = good, y = bad
+// x - y = h[i];
+// x + y = sub[i];
+// x = (h[i] + sub[i]) / 2;
+// y = (sub[i] - h[i]) / 2;
+int ok;
+void dfs2(int curr, int par) {
+    int sum = 0;
+    for(auto i : adj[curr]) {
+        if(i == par) continue;
+        dfs2(i, curr);
         sum += good[i];
     }
     int x = h[curr] + sub[curr];
@@ -72,10 +79,12 @@ int32_t main() {
         }
         sub.clear();
         sub.resize(n + 1);
+        dfs(1, -1);
+        dbg(sub);
         good.clear();
         good.resize(n + 1);
         ok = 1;
-        dfs(1, -1);
+        dfs2(1, -1);
         if(ok) cout << "YES" << endl;
         else cout << "NO" << endl;
     }
