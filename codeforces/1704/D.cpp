2dd33cc6eba98039792ba042bd2eda1c9a1f36e6
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-19 14:42:00 +0530

    Add solution for problem `D - Magical Array` Link: https://codeforces.com/contest/1704/problem/D Tags: constructive algorithms, hashing, implementation, math, *1900 Ref: https://codeforces.com/contest/1704/submission/172636551

---
+++ codeforces/1704/D.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> v(n, vector<int>(m));
+        map<int, int> mp;
+        for(int i=0;i<n;i++) {
+            int s = 0;
+            for(int j=0;j<m;j++) {
+                cin >> v[i][j];
+                s += v[i][j] * (j + 1);
+            }
+            // dbg(s);
+            mp[s] = i + 1;
+        }
+        cout << mp.rbegin()->second << " " << mp.rbegin()->first - mp.begin()->first << endl;
+    }
+}
+// 0 1 2 0 0 2 1 1 0
+// 0 1 3 3 3 5 6 7 7
+
+// 0 1 1 1 2 0 0 2 0
+// 0 1 2 3 5 5 5 7 7
+
+// 0 1 2 0 0 1 2 1 0
+// 0 1 3 3 3 4 6 7 7
