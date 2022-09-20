605fea7451b543a9fd47d8b5d3fddcc445f83b1b
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-21 02:02:00 +0530

    Add solution for problem `I - Coins` Link: https://atcoder.jp/contests/dp/tasks/dp_i Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/35021394

--- atcoder/dp/I.cpp
+++ atcoder/dp/I.cpp
@@ -49,27 +49,11 @@ int32_t main() {
             cin >> v[i];
         }
         dp.clear();
-        dp.resize(n + 1, vector<double>(n + 1));
-        dp[0][0] = 1.0;
-        for(int i=1;i<=n;i++) {
-            dp[i][0] = dp[i - 1][0] * (1 - v[i - 1]);
-        }
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=i;j++) {
-                dp[i][j] = v[i - 1] * dp[i - 1][j - 1] + (1 - v[i - 1]) * dp[i - 1][j];
-            }
-        }
+        dp.resize(n, vector<double>(n + 1, 2));
         double ans = 0;
         for(int i=n/2+1;i<=n;i++) {
-            ans += dp[n][i];
+            ans += solve(0, i);
         }
         cout << ans << endl;
-        
-        // dp.resize(n, vector<double>(n + 1, 2));
-        // double ans = 0;
-        // for(int i=n/2+1;i<=n;i++) {
-        //     ans += solve(0, i);
-        // }
-        // cout << ans << endl;
     }
 }
