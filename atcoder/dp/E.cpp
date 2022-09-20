ce7d6fa9226bd5402bb3759cbb83969536b8e252
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-21 00:00:00 +0530

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/35020786

--- atcoder/dp/E.cpp
+++ atcoder/dp/E.cpp
@@ -23,14 +23,13 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout
 
 int n, w, s;
 vector<int> wt, v;
-// vector<vector<int>> dp;
-vector<int> dp;
-// int solve(int curr, int val) {
-//     if(val <= 0) return 0;
-//     if(curr == n) return INF;
-//     if(dp[curr][val] != -1) return dp[curr][val];
-//     return dp[curr][val] = min(solve(curr + 1, val), wt[curr] + solve(curr + 1, val - v[curr]));
-// }
+vector<vector<int>> dp;
+int solve(int curr, int val) {
+    if(val <= 0) return 0;
+    if(curr == n) return INF;
+    if(dp[curr][val] != -1) return dp[curr][val];
+    return dp[curr][val] = min(solve(curr + 1, val), wt[curr] + solve(curr + 1, val - v[curr]));
+}
 int32_t main() {
     fast;
     int tt = 1;
@@ -47,19 +46,19 @@ int32_t main() {
             s += v[i];
         }
         dp.clear();
-        dp.resize(s + 1, INF);
-        // dp.resize(n + 1, vector<int>(s + 1, INF));
+        dp.resize(n + 1, vector<int>(s + 1, INF));
         for(int i=0;i<=n;i++) {
-            dp[0] = 0;
+            dp[i][0] = 0;
         }
         for(int i=1;i<=n;i++) {
             for(int j=s;j>=1;j--) {
-                if(j - v[i - 1] >= 0) dp[j] = min(dp[j], wt[i - 1] + dp[j - v[i - 1]]);
+                dp[i][j] = dp[i - 1][j];
+                if(j - v[i - 1] >= 0) dp[i][j] = min(dp[i][j], wt[i - 1] + dp[i - 1][j - v[i - 1]]);
             }
         }
         int ans = 0;
         for(int i=0;i<=s;i++) {
-            if(dp[i] <= w) ans = i;
+            if(dp[n][i] <= w) ans = i;
         }
         cout << ans << endl;
 
