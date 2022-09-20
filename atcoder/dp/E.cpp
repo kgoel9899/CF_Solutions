80c32121820fc793eea5cfbc850ef196f5ff1c56
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-20 23:43:00 +0530

    Add solution for problem `E - Knapsack 2` Link: https://atcoder.jp/contests/dp/tasks/dp_e Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/dp/submissions/35020677

--- atcoder/dp/E.cpp
+++ atcoder/dp/E.cpp
@@ -46,27 +46,11 @@ int32_t main() {
             s += v[i];
         }
         dp.clear();
-        dp.resize(n + 1, vector<int>(s + 1, INF));
-        for(int i=0;i<=n;i++) {
-            dp[i][0] = 0;
-        }
-        for(int i=1;i<=n;i++) {
-            for(int j=1;j<=s;j++) {
-                dp[i][j] = dp[i - 1][j];
-                if(j - v[i - 1] >= 0) dp[i][j] = min(dp[i][j], wt[i - 1] + dp[i - 1][j - v[i - 1]]);
-            }
-        }
+        dp.resize(n, vector<int>(s + 1, -1));
         int ans = 0;
         for(int i=0;i<=s;i++) {
-            if(dp[n][i] <= w) ans = i;
+            if(solve(0, i) <= w) ans = i;
         }
         cout << ans << endl;
-
-        // dp.resize(n, vector<int>(s + 1, -1));
-        // int ans = 0;
-        // for(int i=0;i<=s;i++) {
-        //     if(solve(0, i) <= w) ans = i;
-        // }
-        // cout << ans << endl;
     }
 }
