26fb60e78d0acad7d49f60cd416e2505ad1cbb05
Kshitij Goel <kshitijg9899@gmail.com>
2022-09-22 01:21:00 +0530

    Add solution for problem `B - Dima and a Bad XOR` Link: https://codeforces.com/contest/1151/problem/B Tags: bitmasks, brute force, constructive algorithms, dp, *1600 Ref: https://codeforces.com/contest/1151/submission/172994949

--- codeforces/1151/B.cpp
+++ codeforces/1151/B.cpp
@@ -21,78 +21,48 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 
-int n, m;
-vector<vector<int>> v;
-vector<vector<pair<int, int>>> dp;
-pair<int, int> solve(int curr, int xr) {
-    if(curr == n) return {xr != 0, -1};
-    if(dp[curr][xr].first != -1) return dp[curr][xr];
-    int ind = -1, ok = 0;
-    for(int i=0;i<m;i++) {
-        if(solve(curr + 1, xr ^ v[curr][i]).first) {
-            ind = i;
-            ok = 1;
-            break;
-        }
-    }
-    return dp[curr][xr] = {ok, ind};
-}
 int32_t main() {
     fast;
     int tt = 1;
     // cin >> tt;
     while(tt--) {
+        int n, m;
         cin >> n >> m;
-        v.clear();
-        v.resize(n, vector<int>(m));
+        vector<vector<int>> v(n, vector<int>(m));
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
                 cin >> v[i][j];
             }
         }
-        dp.clear();
-        dp.resize(n, vector<pair<int, int>>(1025, {-1, -1}));
-        pair<int, int> ans = solve(0, 0);
-        if(ans.first) {
+        int xr = 0;
+        for(int i=0;i<n;i++) {
+            xr ^= v[i][0];
+        }
+        if(xr > 0) {
             cout << "TAK" << endl;
-            int row = 0, xr = 0;
-            while(row < n) {
-                int ind = dp[row][xr].second;
-                cout << ind + 1 << " ";
-                xr ^= v[row][ind];
-                row++;
+            for(int i=0;i<n;i++) {
+                cout << 1 << " ";
             }
-        } else cout << "NIE" << endl;
-
-        // int xr = 0;
-        // for(int i=0;i<n;i++) {
-        //     xr ^= v[i][0];
-        // }
-        // if(xr > 0) {
-        //     cout << "TAK" << endl;
-        //     for(int i=0;i<n;i++) {
-        //         cout << 1 << " ";
-        //     }
-        //     cout << endl;
-        // } else {
-        //     int row = -1, col;
-        //     for(int i=0;i<n;i++) {
-        //         for(int j=1;j<m;j++) {
-        //             if(v[i][j] != v[i][0]) {
-        //                 row = i;
-        //                 col = j;
-        //             }
-        //         }
-        //     }
-        //     if(row == -1) cout << "NIE" << endl;
-        //     else {
-        //         cout << "TAK" << endl;
-        //         for(int i=0;i<n;i++) {
-        //             if(i == row) cout << col + 1 << " ";
-        //             else cout << 1 << " ";
-        //         }
-        //         cout << endl;
-        //     }
-        // }
+            cout << endl;
+        } else {
+            int row = -1, col;
+            for(int i=0;i<n;i++) {
+                for(int j=1;j<m;j++) {
+                    if(v[i][j] != v[i][0]) {
+                        row = i;
+                        col = j;
+                    }
+                }
+            }
+            if(row == -1) cout << "NIE" << endl;
+            else {
+                cout << "TAK" << endl;
+                for(int i=0;i<n;i++) {
+                    if(i == row) cout << col + 1 << " ";
+                    else cout << 1 << " ";
+                }
+                cout << endl;
+            }
+        }
     }
 }
