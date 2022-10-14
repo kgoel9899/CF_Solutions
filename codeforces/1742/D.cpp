66db31c965884cd9240d5e7a627452825e68f8cf
Kshitij Goel <kshitijg9899@gmail.com>
2022-10-14 10:29:00 +0530

    Add solution for problem `D - Coprime` Link: https://codeforces.com/contest/1742/problem/D Tags: brute force, greedy, number theory, *1100 Ref: https://codeforces.com/contest/1742/submission/176098414

---
+++ codeforces/1742/D.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        map<int, int> m;
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+            m[v[i]] = i + 1;
+        }
+        int ans = -1;
+        for(int i=1;i<=1000;i++) {
+            if(m.find(i) == m.end()) continue;
+            for(int j=1;j<=1000;j++) {
+                if(m.find(j) == m.end()) continue;
+                if(__gcd(i, j) == 1) ans = max(ans, m[i] + m[j]);
+            }
+        }
+        cout << ans << endl;
+    }
+}
