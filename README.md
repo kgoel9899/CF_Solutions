4f76ac628c7e2f9314973bf33ef8891644f03de2
Kshitij Goel <kshitijg9899@gmail.com>
2022-10-16 16:22:00 +0530

    Add solution for problem `B - Making Genome in Berland` Link: https://codeforces.com/contest/638/problem/B Tags: *special problem, dfs and similar, strings, *1500 Ref: https://codeforces.com/contest/638/submission/176475269

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1235 | [C - The Tag Game](https://codeforces.com/contest/813/problem/C) | [GNU C++20 (64)](./codeforces/813/C.cpp) | `dfs and similar` `graphs` `*1700` | Oct/16/2022 17:59 | 
-1234 | [A - Ring road](https://codeforces.com/contest/24/problem/A) | [GNU C++20 (64)](./codeforces/24/A.cpp) | `graphs` `*1400` | Oct/16/2022 17:08 | 
+1236 | [C - The Tag Game](https://codeforces.com/contest/813/problem/C) | [GNU C++20 (64)](./codeforces/813/C.cpp) | `dfs and similar` `graphs` `*1700` | Oct/16/2022 17:59 | 
+1235 | [A - Ring road](https://codeforces.com/contest/24/problem/A) | [GNU C++20 (64)](./codeforces/24/A.cpp) | `graphs` `*1400` | Oct/16/2022 17:08 | 
+1234 | [B - Making Genome in Berland](https://codeforces.com/contest/638/problem/B) | [GNU C++20 (64)](./codeforces/638/B.cpp) | `*special problem` `dfs and similar` `strings` `*1500` | Oct/16/2022 16:22 | 
 1233 | [C - Fox And Names](https://codeforces.com/contest/510/problem/C) | [GNU C++20 (64)](./codeforces/510/C.cpp) | `dfs and similar` `graphs` `sortings` `*1600` | Oct/15/2022 01:33 | 
 1232 | [B - Game on Ranges](https://codeforces.com/contest/1623/problem/B) | [GNU C++20 (64)](./codeforces/1623/B.cpp) | `brute force` `dfs and similar` `implementation` `sortings` `*1100` | Oct/14/2022 01:58 | 
 1231 | [D - Solve The Maze](https://codeforces.com/contest/1365/problem/D) | [GNU C++20 (64)](./codeforces/1365/D.cpp) | `constructive algorithms` `dfs and similar` `dsu` `graphs` `greedy` `implementation` `shortest paths` `*1700` | Oct/14/2022 01:09 | 
---
+++ codeforces/638/B.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<string> v(n);
+        vector<set<int>> adj(26);
+        vector<int> in(26);
+        set<int> used;
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+            used.insert(v[i][0] - 'a');
+            for(int j=1;j<v[i].size();j++) {
+                int x = v[i][j - 1] - 'a';
+                int y = v[i][j] - 'a';
+                if(adj[x].find(y) == adj[x].end()) {
+                    adj[x].insert(y);
+                    in[y]++;
+                }
+                used.insert(x);
+                used.insert(y);
+            }
+        }
+        string ans = "";
+        set<int> done;
+        for(int i=0;i<26;i++) {
+            if(used.find(i) != used.end() && in[i] == 0 && done.find(i) == done.end()) {
+                queue<int> q;
+                q.push(i);
+                while(!q.empty()) {
+                    auto f = q.front();
+                    q.pop();
+                    done.insert(f);
+                    ans += char('a' + f);
+                    for(auto& j : adj[f]) {
+                        in[j]--;
+                        if(in[j] == 0) q.push(j);
+                    }
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+}
