7e346462b4d382fc087f5de5be18a559fdaa8393
Kshitij Goel <kshitijg9899@gmail.com>
2022-11-19 13:30:00 +0530

    Add solution for problem `D - CGCDSSQ` Link: https://codeforces.com/contest/475/problem/D Tags: brute force, data structures, math, *2000 Ref: https://codeforces.com/contest/475/submission/181579340

---
+++ codeforces/475/D.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+const int N = 1e5 + 5;
+const int LOG = 18;
+int sparse[N][LOG];
+int logs[N], v[N];
+int query(int l, int r) {
+    int len = r - l + 1;
+    int lg = logs[len];
+    return __gcd(sparse[l][lg], sparse[r - (1 << lg) + 1][lg]);
+}
+int32_t main() {
+    fast;
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+            sparse[i][0] = v[i];
+        }
+        for(int i=2;i<=n;i++) {
+            logs[i] = logs[i / 2] + 1;
+        }
+        for(int j=1;j<LOG;j++) {
+            for(int i=0;i+(1<<(j-1))-1<n;i++) {
+                sparse[i][j] = __gcd(sparse[i][j - 1], sparse[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+        map<int, int> m;
+        for(int i=0;i<n;i++) {
+            int ind = i;
+            while(ind < n) {
+                int val = query(i, ind);
+                int beg = ind, end = n - 1;
+                int r = ind;
+                while(beg <= end) {
+                    int mid = (beg + end) / 2;
+                    int temp = query(i, mid);
+                    if(temp == val) {
+                        r = mid;
+                        beg = mid + 1;
+                    } else end = mid - 1;
+                }
+                m[val] += r - ind + 1;
+                ind = r + 1;
+            }
+        }
+        int q;
+        cin >> q;
+        while(q--) {
+            int num;
+            cin >> num;
+            cout << m[num] << endl;
+        }
+    }
+}
