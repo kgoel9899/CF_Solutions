7e20998a2faddf1290a95ea2d8ac01047f7e366e
Kshitij Goel <kshitijg9899@gmail.com>
2022-11-24 18:41:00 +0530

    Add solution for problem `C - Thor` Link: https://codeforces.com/contest/705/problem/C Tags: brute force, data structures, implementation, *1600 Ref: https://codeforces.com/contest/705/submission/182356763

--- codeforces/705/C.cpp
+++ codeforces/705/C.cpp
@@ -8,34 +8,62 @@ using namespace std;
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
 const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int query(int l, int r, int node, int st, int end, vector<int>& tree) {
+    if(l > end || r < st) return 0;
+    if(st <= l && r <= end) return tree[node];
+    int mid = (l + r) / 2;
+    return query(l, mid, 2 * node, st, end, tree) + query(mid + 1, r, 2 * node + 1, st, end, tree);
+}
+void update(int l, int r, int node, int ind, int val, vector<int>& tree) {
+    if(ind < l || ind > r) return;
+    if(l == r) {
+        tree[node] = val;
+        return;
+    }
+    int mid = (l + r) / 2;
+    update(l, mid, 2 * node, ind, val, tree);
+    update(mid + 1, r, 2 * node + 1, ind, val, tree);
+    tree[node] = tree[2 * node] + tree[2 * node + 1];
+}
 int32_t main() {
     fast;
-    int t = 1;
-    // cin >> t;
-    while(t--) {
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
         int n, q;
         cin >> n >> q;
-        int ti = 1;
-        vector<vector<int>> adj(n + 1);
-        set<int> s;
-        while(q--) {
-            int type, x;
-            cin >> type >> x;
-            if(type == 1) {
-                adj[x].push_back(ti);
-                s.insert(ti);
-                ti++;
-            } else if(type == 2) {
-                for(auto i : adj[x]) {
-                    if(s.count(i) > 0) s.erase(i);
+        vector<int> tree(4 * q);
+        vector<queue<int>> qq(n);
+        int temp = 0, mx = -1;
+        for(int i=0;i<q;i++) {
+            int t, x;
+            cin >> t >> x;
+            x--;
+            if(t == 1) {
+                update(0, q - 1, 1, temp, 1, tree);
+                qq[x].push(temp);
+                temp++;
+            } else if(t == 2) {
+                while(!qq[x].empty()) {
+                    auto f = qq[x].front();
+                    qq[x].pop();
+                    update(0, q - 1, 1, f, 0, tree);
                 }
-                adj[x].clear();
-            } else {
-                while(s.size() && *s.begin() <= x) {
-                    s.erase(s.begin());
-                }
-            }
-            cout << s.size() << endl;
-        }        
+            } else mx = max(mx, x);
+            cout << query(0, q - 1, 1, mx + 1, q - 1, tree) << endl;
+        }
     }
 }
