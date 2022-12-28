bb4524758a5b7fbcda8dff27962d2c9c904360f5
Kshitij Goel <kshitijg9899@gmail.com>
2022-12-29 01:17:00 +0530

    Add solution for problem `A - Joey Takes Money` Link: https://codeforces.com/contest/1731/problem/A Tags: greedy, math Ref: https://codeforces.com/contest/1731/submission/187075983

--- codeforces/1731/A.cpp
+++ codeforces/1731/A.cpp
@@ -1,40 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,
+tree_order_statistics_node_update> indexed_set;
+#define endl '\n'
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--){
         int n;
-        cin >> n;
-        vector<int> v(n);
-        int p = 1;
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-            p *= v[i];
+        cin>>n;
+        int a[n];
+        ll k=1;
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+            k*=a[i];
         }
-        p += n - 1;
-        cout << p * 2022 << endl;
+        k+=n-1;
+        k*=2022;
+        cout<<k<<endl;
     }
 }
