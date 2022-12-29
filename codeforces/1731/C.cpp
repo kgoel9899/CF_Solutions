9eb5273e19a468d46ffe232ff2a5a7c02e259138
Kshitij Goel <kshitijg9899@gmail.com>
2022-12-30 03:09:00 +0530

    Add solution for problem `C - Even Subarrays` Link: https://codeforces.com/contest/1731/problem/C Tags: bitmasks, brute force, hashing, math, number theory Ref: https://codeforces.com/contest/1731/submission/187189137

--- codeforces/1731/C.cpp
+++ codeforces/1731/C.cpp
@@ -1,49 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n;
+#define ll long long int
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t)
+    {
+        ll n;
         cin >> n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-        }
-        int ans = 0;
-        const int N = n * 2 + 5;
-        vector<int> m(N);
-        m[0] = 1;
+        vector<int> a(n), m(2 * n);
+        for (int i = 0; i < n; i++) 
+            cin >> a[i];
+        ll cnt = 0;
         int curr = 0;
-        for(int i=0;i<n;i++) {
-            curr ^= v[i];
-            for(int j=0;j*j<=N;j++) {
-                if((curr ^ (j * j)) < N) ans += m[(curr ^ (j * j))];
+        m[curr]++;
+        for (int j = 0; j < n; j++)
+        {
+            curr ^= a[j];
+            for (ll i = 0; i * i < 2 * n; i++)
+            {
+                if ((curr ^ (i * i)) < 2 * n)
+                {
+                    cnt += m[curr ^ (i * i)];
+                }
             }
             m[curr]++;
         }
-        cout << (n * (n + 1)) / 2 - ans << endl;
+        ll ans = ((n * (n + 1)) / 2) - cnt;
+        cout << ans << endl;
+        t--;
     }
 }
