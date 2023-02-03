ae9b24a5c09d40edb43b5e97491c5fd1b4e0ec0b
Kshitij Goel <kshitijg9899@gmail.com>
2023-02-03 23:36:00 +0530

    Add solution for problem `D - Distinct Split` Link: https://codeforces.com/contest/1791/problem/D Tags: brute force, greedy, strings Ref: https://codeforces.com/contest/1791/submission/192052221

--- codeforces/1791/D.cpp
+++ codeforces/1791/D.cpp
@@ -1,46 +1,36 @@
-#include<bits/stdc++.h>
+#include "bits/stdc++.h"
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+ 
+#define ll long long
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+ 
+#define            pb                push_back
+#define          sz(a)               (int)a.size()
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
+void solve() {
+    int n; string s; cin >> n >> s;
+    vector<int> cnt(26, 0), p(26, 0);
+    for(auto x: s) cnt[x - 'a']++;
+    int ans = 0;
+    for(auto x: s) {
+        --cnt[x - 'a'];
+        ++p[x - 'a'];
+        int cur = 0;
+        for(int i = 0; i < 26; ++i) {
+            cur += min(1, cnt[i]) + min(1, p[i]);
+        }
+        ans = max(ans, cur);
+    }
+    cout << ans << "\n";
+}   
+ 
 int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n;
-        cin >> n;
-        string s;
-        cin >> s;
-        map<char, int> m, m1;
-        for(auto& i : s) {
-            m[i]++;
-        }
-        int ans = 0;
-        for(int i=0;i<n-1;i++) {
-            m1[s[i]]++;
-            m[s[i]]--;
-            if(m[s[i]] == 0) m.erase(s[i]);
-            ans = max(ans, (int)m.size() + (int)m1.size());
-        }
-        cout << ans << endl;
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
     }
 }
