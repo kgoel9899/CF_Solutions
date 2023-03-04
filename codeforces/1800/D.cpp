6c27a21801b33b2d0d0be908fba44cd3f4d21fa5
Kshitij Goel <kshitijg9899@gmail.com>
2023-03-04 15:54:00 +0530

    Add solution for problem `D - Remove Two Letters` Link: https://codeforces.com/contest/1800/problem/D Tags: constructive algorithms, data structures, greedy, hashing, strings Ref: https://codeforces.com/contest/1800/submission/195950398

--- codeforces/1800/D.cpp
+++ codeforces/1800/D.cpp
@@ -47,6 +47,8 @@ int32_t main() {
             ppow1[i] = (ppow1[i - 1] * p1) % MOD;
             ppow2[i] = (ppow2[i - 1] * p2) % MOD;
         }
+        // dbg(ppow1);
+        dbg(ppow2);
         vector<int> hash1(n), hash2(n);
         hash1[0] = ((s[0] - 'a' + 1) * ppow1[0]) % MOD;
         hash2[0] = ((s[0] - 'a' + 1) * ppow2[0]) % MOD;
@@ -54,6 +56,8 @@ int32_t main() {
             hash1[i] = (hash1[i - 1] + (s[i] - 'a' + 1) * ppow1[i]) % MOD;
             hash2[i] = (hash2[i - 1] + (s[i] - 'a' + 1) * ppow2[i]) % MOD;
         }
+        // dbg(hash1);
+        dbg(hash2);
         set<pair<int, int>> ans;
         for(int i=1;i<n;i++) {
             int x1 = 0;
@@ -61,6 +65,7 @@ int32_t main() {
             int x2 = 0;
             if(i + 1 < n) {
                 x2 = (hash1[n - 1] - hash1[i] + MOD) % MOD;
+                // x2 /= modRecursive(p1, 2, MOD);
                 x2 *= modInverse(modRecursive(p1, 2, MOD));
                 x2 %= MOD;
             }
@@ -69,13 +74,28 @@ int32_t main() {
             int x4 = 0;
             if(i + 1 < n) {
                 x4 = (hash2[n - 1] - hash2[i] + MOD) % MOD;
+                if(i == 8) dbg(x4);
+                // x2 /= modRecursive(p1, 2, MOD);
                 x4 *= modInverse(modRecursive(p2, 2, MOD));
                 x4 %= MOD;
+                if(i == 8) dbg(x3, x4, x3 + x4);
             }
+            // int curr1 = (hash1[i] - (i >= 2 ? hash1[i - 2] : 0) + MOD) % MOD;
+            // curr1 *= ppow1[n - 1 - (i - 1)];
+            // curr1 %= MOD;
+            // dbg(curr1);
+            // int curr2 = (hash2[i] - (i >= 2 ? hash2[i - 2] : 0) + MOD) % MOD;
+            // curr2 *= ppow2[n - 1 - (i - 1)];
+            // curr2 %= MOD;
+            // dbg(curr2);
+            // curr1 = (mult1[n - 1] - curr1 + MOD) % MOD;
+            // curr2 = (mult2[n - 1] - curr2 + MOD) % MOD;
+            // dbg(i, x1, x2, x1 + x2);
             dbg(i, x1 + x2, x3 + x4);
             int val1 = (x1 + x2) % MOD;
             int val2 = (x3 + x4) % MOD;
             ans.insert({val1, val2});
+            // ans.insert({x2});
         }
         cout << ans.size() << endl;
     }
