5bb1d1eb1bb207bfde52e237af05e00d073f1c97
Kshitij Goel <kshitijg9899@gmail.com>
2023-03-19 16:10:00 +0530

    Add solution for problem `B - A and B` Link: https://codeforces.com/contest/1278/problem/B Tags: greedy, math, *1500 Ref: https://codeforces.com/contest/1278/submission/198080431

---
+++ codeforces/1278/B.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int a, b;
+        cin >> a >> b;
+        if(a > b) swap(a, b);
+        if(a == b) {
+            cout << 0 << endl;
+            continue;
+        }
+        // n * (n + 1) / 2 <= b - a
+        int beg = 1, end = b - a;
+        int ans = beg;
+        while(beg <= end) {
+            int mid = (beg + end) / 2;
+            int val = (mid * (mid + 1)) / 2;
+            if(val <= b - a) {
+                ans = mid;
+                beg = mid + 1;
+            } else end = mid - 1;
+        }
+        dbg(ans);
+        a += (ans * (ans + 1)) / 2;
+        int diff = b - a;
+        dbg(diff);
+        dbg(a, b);
+        if(diff == 0) {
+            cout << ans << endl;
+            continue;
+        }
+        int next = ans + 1;
+        if(diff % 2 == next % 2) {
+            cout << ans + 1 << endl;
+            continue;
+        }
+        a += next;
+        next++;
+        ans++;
+        diff = abs(a - b);
+        if(diff % 2 == next % 2) {
+            cout << ans + 1 << endl;
+            continue;
+        }
+        cout << ans + 2 << endl;
+        // dbg(a, b, a - b, next + 1);
+        // a += next + 1;
+        // dbg(a, b, a - b, next + 2);
+        // cout << ans + 2 << endl;
+    }
+}
