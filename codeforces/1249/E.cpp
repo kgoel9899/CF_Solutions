7e9465bae5f06a498ac3435ff9d729126ccd6773
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-17 15:47:00 +0530

    Add solution for problem `E - By Elevator or Stairs?` Link: https://codeforces.com/contest/1249/problem/E Tags: dp, shortest paths, *1700 Ref: https://codeforces.com/contest/1249/submission/202469659

--- codeforces/1249/E.cpp
+++ codeforces/1249/E.cpp
@@ -23,19 +23,6 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout
 
 int n, c;
 vector<int> a, b, dp, dp1, dp2;
-int solve(int curr) {
-    if(curr == 0) return dp[curr] = 0;
-    if(dp[curr] != -1) return dp[curr];
-    int op1 = INF, op2 = INF;
-    int s1 = 0, s2 = 0;
-    for(int i=curr-1;i>=0;i--) {
-        s1 += b[i];
-        s2 += a[i];
-        op1 = min(op1, c + s1 + solve(i));
-        op2 = min(op2, s2 + solve(i));
-    }
-    return dp[curr] = min(op1, op2);
-}
 int32_t main() {
     fast;
     int tt = 1;
@@ -61,22 +48,6 @@ int32_t main() {
             dp1[i] = a[i - 1] + min(dp1[i - 1], dp2[i - 1]);
             dp2[i] = b[i - 1] + min(dp1[i - 1] + c, dp2[i - 1]);
         }
-
-        // dp.clear();
-        // dp.resize(n, INF);
-        // dp[0] = 0;
-        // for(int i=1;i<n;i++) {
-        //     int s1 = 0, s2 = 0;
-        //     for(int j=i-1;j>=0;j--) {
-        //         s1 += b[j];
-        //         s2 += a[j];
-        //         dp[i] = min({dp[i], c + s1 + dp[j], s2 + dp[j]});
-        //     }
-        // }
-
-        // dp.resize(n, -1);
-        // solve(n - 1);
-
         for(int i=0;i<n;i++) {
             cout << min(dp1[i], dp2[i]) << " ";
         }
