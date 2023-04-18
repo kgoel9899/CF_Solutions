a8965d3f966aa9dda873f10e969d43db0ec087b5
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-18 22:08:00 +0530

    Add solution for problem `C - Planar Reflections` Link: https://codeforces.com/contest/1498/problem/C Tags: brute force, data structures, dp, *1600 Ref: https://codeforces.com/contest/1498/submission/202630931

--- codeforces/1498/C.cpp
+++ codeforces/1498/C.cpp
@@ -21,6 +21,7 @@ template<typename T_container, typename T = typename enable_if<!is_same<T_contai
 void dbg_out() { cout << endl; }
 template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 
+
 int n, k;
 vector<vector<vector<int>>> dp;
 int solve(int curr, int age, int dir) {
