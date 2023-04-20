516aa783806b89e0240129e339f5800ee8c030fd
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-21 00:30:00 +0530

    Add solution for problem `C - Phone Numbers` Link: https://codeforces.com/contest/940/problem/C Tags: constructive algorithms, implementation, strings, *1500 Ref: https://codeforces.com/contest/940/submission/202895468

--- codeforces/940/C.cpp
+++ codeforces/940/C.cpp
@@ -7,22 +7,22 @@ using namespace std;
 
 int n, k;
 string s;
-set<char> st;
+set<char> cur;
 
 int32_t main()
 {
     cin>>n>>k;
     cin>>s;
     for(auto it:s)
-        st.insert(it);
-    char first=*(st.begin());
-    char last=*(--st.end());
+        cur.insert(it);
+    char first=*(cur.begin());
+    char last=*(--cur.end());
     if(k>n)
     {
         cout<<s;
         for(int i=1;i<=k-n;i++)
         {
-            cout<<*(st.begin());
+            cout<<*(cur.begin());
         }
         return 0;
     }
@@ -32,7 +32,7 @@ int32_t main()
         {
             if(s[i]!=last)
             {
-                s[i]=*(st.upper_bound(s[i]));
+                s[i]=*(cur.upper_bound(s[i]));
                 break;
             }
             else
