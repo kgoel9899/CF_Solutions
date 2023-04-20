6ed93f04ecd2b15efe381aef8827f2d1584ae219
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-21 00:39:00 +0530

    Add solution for problem `C - Phone Numbers` Link: https://codeforces.com/contest/940/problem/C Tags: constructive algorithms, implementation, strings, *1500 Ref: https://codeforces.com/contest/940/submission/202895921

--- codeforces/940/C.cpp
+++ codeforces/940/C.cpp
@@ -1,58 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-const int INF = 1e18;
+#define int long long
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+int n, k;
+string s;
+set<char> st;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    // cin >> tt;
-    while(tt--) {
-        int n, k;
-        cin >> n >> k;
-        string s;
-        cin >> s;
-        set<char> st;
-        for(auto& i : s) {
-            st.insert(i);
-        }
-        char first=*(st.begin());
-        char last=*(--st.end());
-        if(k>n) cout << s << string(k - n, *st.begin()) << endl;
-        else
+int32_t main()
+{
+    cin>>n>>k;
+    cin>>s;
+    for(auto it:s)
+        st.insert(it);
+    char first=*(st.begin());
+    char last=*(--st.end());
+    if(k>n) cout << s << string(k - n, *st.begin()) << endl;
+    else
+    {
+        for(int i=k-1;i>=0;i--)
         {
-            for(int i=k-1;i>=0;i--)
+            if(s[i]!=last)
             {
-                if(s[i]!=last)
-                {
-                    s[i]=*(st.upper_bound(s[i]));
-                    break;
-                }
-                else
-                {
-                    s[i]=first;
-                }
+                s[i]=*(st.upper_bound(s[i]));
+                break;
             }
-            cout<<s.substr(0, k);
+            else
+            {
+                s[i]=first;
+            }
         }
-        return 0;
+        cout<<s.substr(0, k);
     }
+    return 0;
 } 
