debe510f42027037dc8a202ba67fe3d5536f1c50
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-20 23:59:00 +0530

    Add solution for problem `C - Phone Numbers` Link: https://codeforces.com/contest/940/problem/C Tags: constructive algorithms, implementation, strings, *1500 Ref: https://codeforces.com/contest/940/submission/202893298

--- codeforces/940/C.cpp
+++ codeforces/940/C.cpp
@@ -1,46 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-#define int long long
 
-int n, k;
-string s;
-set<char> cur;
+#define rep(i, n)    for(int i = 0; i < (n); ++i)
+#define repA(i, a, n)  for(int i = a; i <= (n); ++i)
+#define repD(i, a, n)  for(int i = a; i >= (n); --i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+#define fill(a)  memset(a, 0, sizeof (a))
+#define fst first
+#define snd second
+#define mp make_pair
+#define pb push_back
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
-int32_t main()
-{
-    cin>>n>>k;
-    cin>>s;
-    for(auto it:s)
-        cur.insert(it);
-    char first=*(cur.begin());
-    char last=*(--cur.end());
-    if(k>n)
-    {
-        cout<<s;
-        for(int i=1;i<=k-n;i++)
-        {
-            cout<<*(cur.begin());
-        }
-        return 0;
-    }
-    else
-    {
-        for(int i=k-1;i>=0;i--)
-        {
-            if(s[i]!=last)
-            {
-                s[i]=*(cur.upper_bound(s[i]));
-                break;
-            }
-            else
-            {
-                s[i]=first;
-            }
-        }
-        cout<<s.substr(0, k);
-    }
-    return 0;
-} 
+int main() {
+	cin.sync_with_stdio(0); cin.tie(0);
+	cin.exceptions(cin.failbit);
+	int n,k;cin>>n>>k;
+	set<char> t;
+	string s;cin>>s;
+	trav(i,s) t.insert(i);
+	if(k>n){
+		trav(i,s) cout<<i;
+		rep(i,k-n) cout<<*(t.begin());
+	}
+	if(k<n){
+		n = k;
+	}
+	if(k==n){
+		repD(i,n-1,0){
+			if(t.upper_bound(s[i])!=t.end()){
+				s[i] = *(t.upper_bound(s[i]));
+				repA(j,i+1,n-1) s[j] = *(t.begin());
+				break;
+			}
+		}
+		cout<<s.substr(0,n);
+	}
+	return 0;
+}
+
