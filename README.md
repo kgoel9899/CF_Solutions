8fbc2835bb9d515c268ec96a5e9d4f1ce2ea0ff1
Kshitij Goel <kshitijg9899@gmail.com>
2023-04-30 20:02:00 +0530

    Add solution for problem `C - Square Subsets` Link: https://codeforces.com/contest/895/problem/C Tags: bitmasks, combinatorics, dp, math, *2000 Ref: https://codeforces.com/contest/895/submission/204054061

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1446 | [C - Square Subsets](https://codeforces.com/contest/895/problem/C) | [GNU C++20 (64)](./codeforces/895/C.cpp) | `bitmasks` `combinatorics` `dp` `math` `*2000` | Apr/30/2023 19:54 | 
+1446 | [C - Square Subsets](https://codeforces.com/contest/895/problem/C) | [GNU C++20 (64)](./codeforces/895/C.cpp) | `bitmasks` `combinatorics` `dp` `math` `*2000` | Apr/30/2023 20:02 | 
 1445 | [D - Kefa and Dishes](https://codeforces.com/contest/580/problem/D) | [GNU C++20 (64)](./codeforces/580/D.cpp) | `bitmasks` `dp` `*1800` | Apr/30/2023 16:51 | 
 1444 | [D - Multicolored Cars](https://codeforces.com/contest/818/problem/D) | [GNU C++20 (64)](./codeforces/818/D.cpp) | `data structures` `implementation` `*1700` | Apr/29/2023 23:56 | 
 1443 | [B - Karina and Array](https://codeforces.com/contest/1822/problem/B) | [GNU C++20 (64)](./codeforces/1822/B.cpp) | `greedy` `math` `sortings` `*800` | Apr/28/2023 12:30 | 
--- codeforces/895/C.cpp
+++ codeforces/895/C.cpp
@@ -1,105 +1,85 @@
-// https://codeforces.com/contest/895/submission/40238041
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MOD 1000000007
+#define mod 998244353
+// #define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
+// const int INF = 1e18;
 
-const int N=1e5+5;
-const int MOD=1e9+7;
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-int n;
-long long freq[N];
-int cache[71][1<<19];
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int n, sz;
 vector<int> primes;
-
-long long pow(long long a, long long b, long long m)
-{
-	long long ans=1;
-	while(b)
-	{
-		if(b&1)
-			ans=(ans*a)%m;
-		b/=2;
-		a=(a*a)%m;
-	}
-	return ans;
+int dp[71][1ll << 19];
+const int N = 71;
+map<int, int> m;
+bool prime(int num) {
+    for(int i=2;i*i<=num;i++) {
+        if(num % i == 0) return false;
+    }
+    return true;
 }
-
-bool isprime(int k)
-{
-	for(int i=2;i<k;i++)
-	{
-		if(k%i==0)
-			return 0;
-	}
-	return 1;
+int get(int num, int mask) {
+    for(int i=0;i<sz;i++) {
+        int ct = 0;
+        while(num % primes[i] == 0) {
+            num /= primes[i];
+            ct++;
+        }
+        if(ct % 2) mask ^= (1 << i);
+    }
+    return mask;
 }
-
-void preprocess()
-{
-	for(int i=2;i<=70;i++)
-	{
-		if(isprime(i))
-			primes.push_back(i);
-	}
+int modRecursive(int a, int b, int c) {
+    if(b == 0) return 1;
+    int temp = modRecursive(a, b / 2, c);
+    if(b % 2 == 0) return ((long long)temp * temp) % c;
+    else return ((long long)a * (((long long)temp * temp) % c)) % c;
 }
-
-int newmask(int k, int bitmask)
-{
-	for(int i=0;i<primes.size();i++)
-	{
-		int ct=0;
-		while(k%primes[i]==0)
-		{
-			k/=primes[i];
-			ct++;
-		}
-		if(ct%2)
-			bitmask ^= (1<<i);
-	}
-	return bitmask;
+int solve(int curr, int mask) {
+    if(curr == N) return !mask;
+    if(dp[curr][mask] != -1) return dp[curr][mask];
+    if(m[curr] == 0) return dp[curr][mask] = solve(curr + 1, mask);
+    int new_mask = get(curr, mask); // if we take idx'th element odd number of times
+    int ct = modRecursive(2, m[curr] - 1, MOD); // number of ways of picking odd/even number of idx'th element
+    int op1 = ((long long)ct * solve(curr + 1, new_mask)) % MOD;
+    int op2 = ((long long)ct * solve(curr + 1, mask)) % MOD;
+    return dp[curr][mask] = ((long long)op1 + op2) % MOD;
 }
-
-int dp(int i, int bitmask)
-{
-	if(i==71)
-	{
-		if(!bitmask)
-			return 1;
-		return 0;
-	}
-	if(cache[i][bitmask]!=-1)
-		return cache[i][bitmask];
-	int storemask=newmask(i, bitmask);
-	long long ans1=0;
-	if(freq[i]!=0)
-		ans1=pow(2LL, freq[i]-1, MOD) * dp(i+1, storemask);
-	ans1%=MOD;
-	long long ans2=1;
-	if(freq[i]!=0)
-		ans2=(pow(2LL, freq[i]-1, MOD)) * dp(i+1, bitmask); 
-	else
-		ans2=dp(i+1, bitmask);
-	ans2%=MOD;
-	cache[i][bitmask]=(ans1+ans2)%MOD;
-	return cache[i][bitmask];
-}
-
-int32_t main()
-{
-	IOS;
-	memset(cache, -1, sizeof(cache));
-	preprocess();
-	cin>>n;
-	for(int i=1;i<=n;i++)
-	{
-		int k;
-		cin>>k;
-		freq[k]++;
-	}
-	int ans=dp(1, 0);
-	cout<<ans-1;
-	return 0;
+int32_t main() {
+    fast;
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        cin >> n;
+        m.clear();
+        for(int i=0;i<n;i++) {
+            int x;
+            cin >> x;
+            m[x]++;
+        }
+        primes.clear();
+        for(int i=2;i<N;i++) {
+            if(prime(i)) primes.push_back(i);
+        }
+        sz = primes.size();
+        memset(dp, -1, sizeof dp);
+        int ans = solve(0, 0) - 1;
+        ans += MOD;
+        ans %= MOD;
+        cout << ans << endl;
+    }
 }
