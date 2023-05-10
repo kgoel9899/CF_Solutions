305a219dcce75187574f07be48f45b1b6801572d
Kshitij Goel <kshitijg9899@gmail.com>
2023-05-10 23:49:00 +0530

    Add solution for problem `B - LuoTianyi and the Table` Link: https://codeforces.com/contest/1825/problem/B Tags: greedy, math Ref: https://codeforces.com/contest/1825/submission/205360681

--- codeforces/1825/B.cpp
+++ codeforces/1825/B.cpp
@@ -1,46 +1,25 @@
 #include<bits/stdc++.h>
+#define ll long long
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n, m;
-        cin >> n >> m;
-        vector<int> v(n * m);
-        for(int i=0;i<n*m;i++) {
-            cin >> v[i];
-        }
-        sort(all(v));
-        int ans1 = (v.back() - v[0]) * (n * m - n - m + 1);
-        ans1 += (max(n, m) - 1) * (v.back() - v[0]);
-        ans1 += (min(n, m) - 1) * (v.back() - v[1]);
-        reverse(all(v));
-        int ans2 = (v.back() - v[0]) * (n * m - n - m + 1);
-        ans2 += (max(n, m) - 1) * (v.back() - v[0]);
-        ans2 += (min(n, m) - 1) * (v.back() - v[1]);
-        ans2 = abs(ans2);
-        cout << max(ans1, ans2) << endl;
-    }
+const int mxn=2e5+5;
+ll a[mxn];
+inline void solve(){
+	ll n,m;
+	cin>>n>>m;
+	for(int i=1;i<=n*m;++i)cin>>a[i];
+	sort(a+1,a+n*m+1);
+	if(n>m)swap(n,m);
+	if(n==1)cout<<(m-1)*(a[n*m]-a[1])<<'\n';
+	else{
+		ll ans1=(n*m-1)*(a[n*m])-a[1]*(n*(m-1))-a[2]*(n-1);
+		ll ans2=a[n*m]*(n*(m-1))+a[n*m-1]*(n-1)-a[1]*(n*m-1);
+		cout<<max(ans1,ans2)<<'\n';
+	}
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(0),cout.tie(0);
+	int T;cin>>T;
+	for(;T--;)solve();
+	return 0;
 }
