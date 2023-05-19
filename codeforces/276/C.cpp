910ad3c9be6557d117616d13388762143e4baf34
Kshitij Goel <kshitijg9899@gmail.com>
2023-05-19 23:52:00 +0530

    Add solution for problem `C - Little Girl and Maximum Sum` Link: https://codeforces.com/contest/276/problem/C Tags: data structures, greedy, implementation, sortings, *1500 Ref: https://codeforces.com/contest/276/submission/206558874

--- codeforces/276/C.cpp
+++ codeforces/276/C.cpp
@@ -1,51 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAX 100001
 #define MOD 1000000007
+#define mod 998244353
 #define int long long
-#define ld long double
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
 #define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
 int32_t main() {
     fast;
-    int t = 1;
-    // cin >> t;
-    while(t--) {
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
         int n, q;
         cin >> n >> q;
         vector<int> v(n);
         for(int i=0;i<n;i++) {
             cin >> v[i];
         }
-        vector<int> pref(n + 1);
+        vector<int> pref(n + 2);
         while(q--) {
-            int a, b;
-            cin >> a >> b;
-            a--;
-            b--;
-            pref[a]++;
-            pref[b + 1]--;
+            int l, r;
+            cin >> l >> r;
+            pref[l]++;
+            pref[r + 1]--;
         }
-        for(int i=1;i<=n;i++) {
+        for(int i=1;i<n+2;i++) {
             pref[i] += pref[i - 1];
         }
-        vector<int> temp;
-        for(int i=0;i<=n;i++) {
-            if(pref[i] > 0) temp.push_back(pref[i]);
-        }
-        sort(v.begin(), v.end(), greater<int>());
-        sort(temp.begin(), temp.end(), greater<int>());
-        // for(auto i : v) {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-        // for(auto i : temp) {
-        //     cout << i << " ";
-        // }
-        // cout << endl;
-        int ans = 0, x = 0;
-        for(auto i : temp) {
-            ans += i * v[x++];
+        sort(all(pref), greater<int>());
+        sort(all(v), greater<int>());
+        int ans = 0;
+        for(int i=0;i<n;i++) {
+            ans += v[i] * pref[i];
         }
         cout << ans << endl;
     }
