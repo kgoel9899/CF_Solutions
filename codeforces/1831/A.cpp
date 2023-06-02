6ded0860585ac86a125cd393d87ed6e77d2c492f
Kshitij Goel <kshitijg9899@gmail.com>
2023-06-02 23:53:00 +0530

    Add solution for problem `A - Twin Permutations` Link: https://codeforces.com/contest/1831/problem/A Tags: constructive algorithms Ref: https://codeforces.com/contest/1831/submission/208202339

--- codeforces/1831/A.cpp
+++ codeforces/1831/A.cpp
@@ -1,40 +1,21 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-        }
-        for(int i=0;i<n;i++) {
-            cout << n - v[i] + 1 << " ";
-        }
-        cout << endl;
+typedef long long ll;
+void tc(){
+    ll n;
+    cin>>n;
+    for(ll i=0;i<n;i++){
+        ll x;
+        cin>>x;
+        cout<<n+1-x<<' ';
     }
+    cout<<'\n';
+}
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(0);
+    ll t; cin>>t; while(t--)
+        tc();
+    return 0;
 }
