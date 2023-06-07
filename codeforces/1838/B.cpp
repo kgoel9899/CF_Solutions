0f2a5c6a9c513997b72f8d55abe84b8a72b8d13c
Kshitij Goel <kshitijg9899@gmail.com>
2023-06-07 23:59:00 +0530

    Add solution for problem `B - Minimize Permutation Subarrays` Link: https://codeforces.com/contest/1838/problem/B Tags: constructive algorithms, math Ref: https://codeforces.com/contest/1838/submission/208925935

--- codeforces/1838/B.cpp
+++ codeforces/1838/B.cpp
@@ -1,41 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+#define N 200010
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+int idx[N];
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+int main() {
 
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+    int t; cin >> t;
+    for(int tc = 1; tc <= t; ++tc) {
 
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n;
-        cin >> n;
-        vector<int> v(n);
-        map<int, int> m;
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-            m[v[i]] = i + 1;
+        int n; cin >> n;
+
+        for(int i = 1; i <= n; ++i) {
+            int x; cin >> x;
+            idx[x] = i;
         }
-        if(m[n] > min(m[1], m[2]) && m[n] < max(m[1], m[2])) cout << m[1] << " " << m[1] << endl;
-        else if(m[n] > max(m[1], m[2])) cout << m[n] << " " << max(m[1], m[2]) << endl;
-        else cout << m[n] << " " << min(m[1], m[2]) << endl;
+
+        if(idx[n] < min(idx[1], idx[2])) {
+            cout << idx[n] << ' ' << min(idx[1], idx[2]) << '\n';
+        } else if(idx[n] > max(idx[1], idx[2])) {
+            cout << idx[n] << ' ' << max(idx[1], idx[2]) << '\n';
+        } else {
+            cout << idx[1] << ' ' << idx[2] << '\n';
+        }
     }
 }
