4c3c8fe65b8958ad65638d3fb553d75feffd375c
Kshitij Goel <kshitijg9899@gmail.com>
2023-06-12 23:59:00 +0530

    Add solution for problem `D - Paths on the Tree` Link: https://codeforces.com/contest/1746/problem/D Tags: dfs and similar, dp, greedy, sortings, trees, *1900 Ref: https://codeforces.com/contest/1746/submission/209486955

--- codeforces/1746/D.cpp
+++ codeforces/1746/D.cpp
@@ -1,84 +1,58 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
 #define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+#define sz(x) (int)(x).size()
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int n, k;
-vector<int> s;
 vector<vector<int>> adj;
-vector<map<int, int>> dp;
-bool compare(pair<int, int>& a, pair<int, int>& b) {
-    int x = a.second - a.first;
-    int y = b.second - b.first;
-    return x > y;
+vector<map<int, int>> score;
+vector<int> s;
+
+int dfs(int node, int k) {
+	// score assuming k paths travel through node.
+	if(adj[node].empty()) return k * s[node];
+	if(score[node].find(k) != score[node].end()) return score[node][k];
+	vector<pair<int, int>> v;
+	int c = sz(adj[node]);
+	for(auto next : adj[node]) {
+		pair<int, int> p;
+		p.first = dfs(next, floor(k*1.0/c));
+		p.second = dfs(next, ceil(k*1.0/c));
+		v.push_back(p);
+	}
+	int rem = k % c; // ceil needed this many times.
+	int ans = 0;
+	sort(v.begin(), v.end(), [&](pair<int, int> a, pair<int, int> b) {
+		return (a.second - a.first) > (b.second - b.first);
+	});
+	for(int i = 0; i < sz(v); i++) {
+		if(i < rem) ans += v[i].second;
+		else ans += v[i].first;
+	}
+	score[node][k] = ans + (s[node] * k);
+	return score[node][k];
 }
-int dfs(int curr, int paths) {
-    if(adj[curr].size() == 0) {
-        dbg(curr, paths); return paths * s[curr];
-    }
-    dbg("###### ", curr, paths);
-    if(dp[curr].find(paths) != dp[curr].end()) return dp[curr][paths];
-    vector<pair<int, int>> v;
-    int ct = adj[curr].size();
-    for(auto& i : adj[curr]) {
-        pair<int, int> here;
-        dbg("1111111", i, curr);
-        here.first = dfs(i, floor(paths * 1.0 / ct));
-        dbg("2222222", i, curr);
-        here.second = dfs(i, ceil(paths * 1.0  / ct));
-        dbg(curr, paths, here);
-        v.push_back(here);
-    }
-    sort(all(v), compare);
-    int extra = paths % ct;
-    int ans = 0;
-    assert(ct == v.size());
-    for(int i=0;i<ct;i++) {
-        if(i < extra) ans += v[i].second;
-        else ans += v[i].first;
-    }
-    dbg(ans, curr, paths);
-    return dp[curr][paths] = ans + paths * s[curr];
-}
+
 int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        cin >> n >> k;
-        adj.clear();
-        adj.resize(n + 1);
-        for(int i=2;i<=n;i++) {
-            int p;
-            cin >> p;
-            adj[p].push_back(i);
-        }
-        dbg(adj);
-        s.clear();
-        s.resize(n + 1);
-        for(int i=1;i<=n;i++) {
-            cin >> s[i];
-        }
-        dp.clear();
-        dp.resize(n + 1);
-        cout << dfs(1, k) << endl;
-    }
+	cin.tie(0)->sync_with_stdio(0);
+	
+	int t = 1;
+	cin >> t;
+	while(t--) {
+		int n, k;
+		cin >> n >> k;
+		adj.clear(); adj.resize(n);
+		score.clear(); score.resize(n);
+		s.clear(); s.resize(n);
+		for(int i = 1; i < n; i++) {
+			int p; cin >> p;
+			adj[p - 1].push_back(i);
+		}
+		for(int i = 0; i < n; i++) {
+			cin >> s[i];
+		}
+		cout << dfs(0, k) << '\n';
+	}
+	
+	return 0;
 }
