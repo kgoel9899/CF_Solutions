96be062f9a2ab4a85d8443eae70cd7920169a75b
Kshitij Goel <kshitijg9899@gmail.com>
2023-06-17 23:58:00 +0530

    Add solution for problem `D - Chloe and pleasant prizes` Link: https://codeforces.com/contest/743/problem/D Tags: dfs and similar, dp, graphs, trees, *1800 Ref: https://codeforces.com/contest/743/submission/209994059

---
+++ codeforces/743/D.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int n, ans;
+vector<int> v, sub;
+vector<vector<int>> adj;
+void dfs(int curr, int par) {
+    sub[curr] = v[curr];
+    for(auto& i : adj[curr]) {
+        if(i == par) continue;
+        dfs(i, curr);
+        sub[curr] += sub[i];
+    }
+}
+int dfs2(int curr, int par) {
+    vector<int> temp;
+    for(auto& i : adj[curr]) {
+        if(i == par) continue;
+        temp.push_back(dfs2(i, curr));
+    }
+    sort(all(temp));
+    reverse(all(temp));
+    if(temp.size() == 0) return v[curr];
+    if(temp.size() >= 2) ans = max(ans, temp[0] + temp[1]);
+    return max(sub[curr], temp[0]);
+}
+int32_t main() {
+    fast;
+    int tt = 1;
+    // cin >> tt;
+    while(tt--) {
+        cin >> n;
+        v.clear();
+        v.resize(n + 1);
+        for(int i=1;i<=n;i++) {
+            cin >> v[i];
+        }
+        adj.clear();
+        adj.resize(n + 1);
+        for(int i=0;i<n-1;i++) {
+            int a, b;
+            cin >> a >> b;
+            adj[a].push_back(b);
+            adj[b].push_back(a);
+        }
+        sub.clear();
+        sub.resize(n + 1);
+        dfs(1, 0);
+        ans = -INF;
+        dfs2(1, 0);
+        if(ans == -INF) cout << "Impossible" << endl;
+        else cout << ans << endl;
+    }
+}
