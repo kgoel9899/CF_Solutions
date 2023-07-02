be0f82c41525f0d37d76f74d70706fec2b0d8456
Kshitij Goel <kshitijg9899@gmail.com>
2023-07-02 23:57:00 +0530

    Add solution for problem `B - Come Together` Link: https://codeforces.com/contest/1845/problem/B Tags: geometry, implementation, math Ref: https://codeforces.com/contest/1845/submission/211863090

--- codeforces/1845/B.cpp
+++ codeforces/1845/B.cpp
@@ -1,38 +1,52 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
+#define fore(i, l, r) for(int i = int(l); i < int(r); i++)
+#define sz(a) int((a).size())
+
+#define x first
+#define y second
+
+typedef long long li;
+typedef pair<int, int> pt;
+
+pt A, B, C;
+
+inline bool read() {
+	if(!(cin >> A.x >> A.y))
+		return false;
+	cin >> B.x >> B.y;
+	cin >> C.x >> C.y;
+	return true;
+}
+
+int dist(const pt &A, const pt &B) {
+	return abs(A.x - B.x) + abs(A.y - B.y);
+}
+
+inline void solve() {
+	cout << (dist(A, B) + dist(A, C) - dist(B, C)) / 2 + 1 << endl;
+}
+
+int main() {
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	int tt = clock();
 #endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int xa, ya, xb, yb, xc, yc;
-        cin >> xa >> ya >> xb >> yb >> xc >> yc;
-        int ans = 0;
-        if(min(xb, xc) >= xa) ans += abs(xa - min(xb, xc));
-        else if(max(xb, xc) <= xa) ans += abs(xa - max(xb, xc));
-        if(min(yb, yc) >= ya) ans += abs(ya - min(yb, yc));
-        else if(max(yb, yc) <= ya) ans += abs(ya - max(yb, yc));
-        cout << ans + 1 << endl;
-    }
+	ios_base::sync_with_stdio(false);
+	cin.tie(0), cout.tie(0);
+	cout << fixed << setprecision(15);
+	
+	int t; cin >> t;
+	while (t--) {
+		read();
+		solve();
+		
+#ifdef _DEBUG
+		cerr << "TIME = " << clock() - tt << endl;
+		tt = clock();
+#endif
+	}
+	return 0;
 }
