4b711a74cc4a6718c86bf038d8791c4764263980
Kshitij Goel <kshitijg9899@gmail.com>
2023-07-06 22:36:00 +0530

    Add solution for problem `D - Wooden Toy Festival` Link: https://codeforces.com/contest/1840/problem/D Tags: binary search, greedy, sortings, *1400 Ref: https://codeforces.com/contest/1840/submission/212450299

--- codeforces/1840/D.cpp
+++ codeforces/1840/D.cpp
@@ -1,59 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+ 
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
+ 
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
         int n;
         cin >> n;
-        vector<int> v(n);
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
         }
-        sort(all(v));
-        int beg = 0, end = 1e9, ans = 1e9;
-        while(beg <= end) {
-            int mid = (beg + end) / 2;
-            int l = n, r = 0;
-            for(int i=0;i<n;i++) {
-                if(v[i] - v[0] > 2 * mid) {
-                    l = i - 1;
-                    break;
-                }
+        sort(a.begin(), a.end());
+        int l = -1, r = 1e9;
+        while (r - l > 1) {
+            int m = (l + r) >> 1;
+            int i = 0;
+            while (i + 1 < a.size() && a[i + 1] - a[0] <= 2 * m) {
+                ++i;
             }
-            for(int i=n-1;i>=0;i--) {
-                if(v[n - 1] - v[i] > 2 * mid) {
-                    r = i + 1;
-                    break;
-                }
+            int j = n - 1;
+            while (j - 1 >= 0 && a.back() - a[j - 1] <= 2 * m) {
+                --j;
             }
-            if(l + 2 >= r || v[r - 1] - v[l + 1] <= 2 * mid) {
-                ans = mid;
-                end = mid - 1;
-            } else beg = mid + 1;
+            ++i; --j;
+            if (i > j || a[j] - a[i] <= 2 * m) {
+                r = m;
+            } else {
+                l = m;
+            }
         }
-        cout << ans << endl;
+        cout << r << "\n";
     }
+    return 0;
 }
