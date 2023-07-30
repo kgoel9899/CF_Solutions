527a54fa3e287990519a9acfb8320913ca5b289c
Kshitij Goel <kshitijg9899@gmail.com>
2023-07-31 00:02:00 +0530

    Add solution for problem `E2 - Rudolf and Snowflakes (hard version)` Link: https://codeforces.com/contest/1846/problem/E2 Tags: binary search, brute force, implementation, math, *1800 Ref: https://codeforces.com/contest/1846/submission/216488662

--- codeforces/1846/E2.cpp
+++ codeforces/1846/E2.cpp
@@ -1,66 +1,59 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+using LL = long long;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+set<long long> nums;
 
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+int main() {
 
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    
-    while(tt--) {
-        int n;
+    for (long long k = 2; k <= 1000000; ++k) {
+        long long val = 1 + k;
+        long long p = k*k;
+        for (int cnt = 3; cnt <= 63; ++cnt) {
+            val += p;
+            if (val > 1e18) break;
+            nums.insert(val);
+            if (p > (long long)(1e18) / k) break;
+            p *= k;
+        }
+    }
+
+
+
+    int _ = 0, __ = 1;
+    cin >> __;
+
+    for (int _ = 0; _ < __; ++_) {
+        long long n;
         cin >> n;
-        if(n < 7) {
+        if (n < 3)
+        {
             cout << "NO" << endl;
             continue;
         }
-        const int MX = 1e18;
-        int done = 0;
-        for(int p=2;p<63;p++) {
-            int beg = 2, end = 1e9;
-            while(beg <= end) {
-                int r = (beg + end) / 2;
-                int val = 1 + r + r * r;
-                int fact = r * r;
-                int too = 0;
-                for(int i=3;i<=p;i++) {
-                    if(fact > MX / r) {
-                        too = 1;
-                        break;
-                    }
-                    fact *= r;
-                    val += fact;
-                }
-                if(too == 0) {
-                    if(val == n) {
-                        done = 1;
-                        break;
-                    } else if(val > n) end = r - 1;
-                    else beg = r + 1;
-                } else end = r - 1;
+        long long d = 4*n - 3;
+        long long sq = sqrt(d);
+        long long sqd = -1;
+        for (long long i = max(0ll, sq - 5); i <= sq + 5; ++i) {
+            if (i*i == d)
+            {
+                sqd = i;
+                break;
             }
-            if(done) break;
         }
-        if(done) cout << "YES" << endl;
+        if (sqd != -1 && (sqd - 1) % 2 == 0 && (sqd - 1) / 2 > 1)
+        {
+            cout << "YES" << endl;
+            continue;
+        }
+
+        if (nums.count(n)) cout << "YES" << endl;
         else cout << "NO" << endl;
+
     }
+
+
+    return 0;
 }
