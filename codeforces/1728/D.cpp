954db0fb684bef639a0df9bc8fbcbba7f7661e85
Kshitij Goel <kshitijg9899@gmail.com>
2023-08-07 22:40:00 +0530

    Add solution for problem `D - Letter Picking` Link: https://codeforces.com/contest/1728/problem/D Tags: constructive algorithms, dp, games, two pointers, *1800 Ref: https://codeforces.com/contest/1728/submission/217742265

--- codeforces/1728/D.cpp
+++ codeforces/1728/D.cpp
@@ -79,10 +79,9 @@ int solve(int i, int j) {
         // Alice wins after picking j and no matter what Bob chooses afterwards (i or j - 1)
         return dp[i][j] = 1;
     }
-    
+
     if((ans1 == 2 || ans2 == 2) && (ans3 == 2 || ans4 == 2)) {
         // bob wins in atleast 1 scenario when Alice picks i or j
-        assert(ans1 + ans2 + ans3 + ans4 == 8);
         return dp[i][j] = 2;
     }
 
