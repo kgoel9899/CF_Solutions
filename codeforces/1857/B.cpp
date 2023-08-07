d08c845acc10e39cf2b33eaf5ba73a53a5ce6df5
Kshitij Goel <kshitijg9899@gmail.com>
2023-08-08 00:00:00 +0530

    Add solution for problem `B - Maximum Rounding` Link: https://codeforces.com/contest/1857/problem/B Tags: greedy, implementation, math Ref: https://codeforces.com/contest/1857/submission/217754327

--- codeforces/1857/B.cpp
+++ codeforces/1857/B.cpp
@@ -1,50 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
 #define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        string s;
-        cin >> s;
-        int n = s.size(), on = 0, last = n;
-        for(int i=n-1;i>=0;i--) {
-            if(s[i] >= '5') on = 1;
-            else if(on) {
-                if(s[i] == '4') continue;
-                else {
-                    s[i]++;
-                    last = i;
-                    on = 0;
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int t;cin>>t;
+    while(t--)
+    {
+        string s;cin>>s;
+        bool ok=0;
+        for(int i=0;i<s.size();++i)
+        {
+            if(s[i]>='5')
+            {
+                ok=1;
+                int pos=i;
+                while(pos)
+                {
+                    s[pos-1]++;
+                    if(s[pos-1]<'5') break;
+                    --pos;
+                }
+                if(pos==0)
+                {
+                    cout<<'1';for(int i=0;i<s.size();++i) cout<<'0'; cout<<'\n';
+                    break;
+                }
+                else
+                {
+                    for(int i=pos;i<s.size();++i) s[i]='0';
+                    cout<<s<<'\n';break;
                 }
             }
         }
-        dbg(on, last);
-        for(int i=last+1;i<n;i++) {
-            s[i] = '0';
-        }
-        if(on) s = '1' + string(n, '0');
-        cout << s << endl;
+        if(!ok) {cout<<s<<'\n';}
     }
+    return 0;
 }
