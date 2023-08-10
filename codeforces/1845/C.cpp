1fcd14c516e818ba54ab825a2b230fa7173d0680
Kshitij Goel <kshitijg9899@gmail.com>
2023-08-10 23:59:00 +0530

    Add solution for problem `C - Strong Password` Link: https://codeforces.com/contest/1845/problem/C Tags: binary search, dp, greedy, strings, *1400 Ref: https://codeforces.com/contest/1845/submission/218208434

--- codeforces/1845/C.cpp
+++ codeforces/1845/C.cpp
@@ -1,59 +1,118 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#ifdef LOCAL
+	#define eprintf(...) {fprintf(stderr, __VA_ARGS__);fflush(stderr);}
 #else
-#define dbg(...)
+	#define eprintf(...) 42
 #endif
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template<typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {
+	return (ull)rng() % B;
+}
 
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
 
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        string s;
-        cin >> s;
-        int m;
-        cin >> m;
-        string l, r;
-        cin >> l >> r;
-        map<int, vector<int>> mp;
-        int n = s.size();
-        for(int i=0;i<n;i++) {
-            mp[s[i] - '0'].push_back(i);
-        }
-        dbg(mp);
-        int ind = 0, done = 0;
-        for(int i=0;i<m;i++) {
-            int x = l[i] - '0', y = r[i] - '0', temp = 0;
-            dbg(i, x, y, temp, ind);
-            for(int j=x;j<=y;j++) {
-                auto it = lower_bound(all(mp[j]), ind);
-                if(it == mp[j].end()) {
-                    done = 1;
-                    break;
-                }
-                temp = max(temp, *it);
-            }
-            if(done) break;
-            ind = temp + 1;
-        }
-        if(done) cout << "YES" << endl;
-        else cout << "NO" << endl;
-    }
+clock_t startTime;
+double getCurrentTime() {
+	return (double)(clock() - startTime) / CLOCKS_PER_SEC;
+}
+
+const int N = 300300;
+const int A = 10;
+char s[N];
+char L[N], R[N];
+int n, m;
+int nxt[N][A];
+
+void solve() {
+	scanf("%s", s);
+	n = strlen(s);
+	scanf("%d", &m);
+	scanf(" %s %s", L, R);
+	for (int c = 0; c < A; c++)
+		nxt[n][c] = n + 1;
+	for (int i = n - 1; i >= 0; i--) {
+		for (int c = 0; c < A; c++)
+			nxt[i][c] = nxt[i + 1][c];
+		nxt[i][(int)(s[i] - '0')] = i + 1;
+	}
+	int p = 0;
+	for (int i = 0; i < m; i++) {
+		int q = -1;
+		for (int c = (int)(L[i] - '0'); c <= (int)(R[i] - '0'); c++)
+			q = max(q, nxt[p][c]);
+		if (q == n + 1) {
+			printf("YES\n");
+			return;
+		}
+		p = q;
+	}
+	printf("NO\n");
+}
+
+int main()
+{
+	startTime = clock();
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+
+	int t;
+	scanf("%d", &t);
+	for (int i = 1; i <= t; i++) {
+		eprintf("--- Case #%d start ---\n", i);
+		//printf("Case #%d: ", i);
+
+		solve();
+
+		//printf("%lld\n", (ll)solve());
+
+		/*
+		if (solve()) {
+			printf("Yes\n");
+		} else {
+			printf("No\n");
+		}
+		*/
+
+		eprintf("--- Case #%d end ---\n", i);
+		eprintf("time = %.5lf\n", getCurrentTime());
+		eprintf("++++++++++++++++++++\n");
+	}
+
+
+	return 0;
 }
