d6299357922308f2a9d9ac1f358cc0eb44b26c97
Kshitij Goel <kshitijg9899@gmail.com>
2023-09-01 23:58:00 +0530

    Add solution for problem `B - Two Binary Strings` Link: https://codeforces.com/contest/1861/problem/B Tags: constructive algorithms, dp, greedy Ref: https://codeforces.com/contest/1861/submission/221486509

---
+++ codeforces/1861/B.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        string a, b;
+        cin >> a >> b;
+        int n = a.size();
+        if(a[0] != b[0] || a[n - 1] != b[n - 1]) {
+            cout << "NO" << endl;
+            continue;
+        }
+        int ok = 0;
+        for(int i=0;i<n-1;i++) {
+            if(a[i] == '0' && a[i + 1] == '1' && b[i] == '0' && b[i + 1] == '1') {
+                ok = 1;
+                break;
+            }
+        }
+        if(ok) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+}
