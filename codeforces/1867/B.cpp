5c3a9c80ed58fc4508e5a61ff380129873fc69bc
Kshitij Goel <kshitijg9899@gmail.com>
2023-09-12 15:13:00 +0530

    Add solution for problem `B - XOR Palindromes` Link: https://codeforces.com/contest/1867/problem/B Tags: bitmasks, constructive algorithms, strings Ref: https://codeforces.com/contest/1867/submission/223064771

---
+++ codeforces/1867/B.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int a = 0, b = 0;
+        int l = 0, r = n - 1;
+        while(l < r) {
+            if(s[l] == s[r]) b += 2;
+            else a++;
+            l++;
+            r--;
+        }
+        dbg(a, b);
+        string ans = string(n + 1, '0');
+        for(int i=0;i<=n;i++) {
+            if(a == 0) {
+                if(i % 2 == 0) ans[i] = '1';
+                else if(n % 2) ans[i] = '1';
+            } else if(i == a) {
+                for(int j=i;j<=n&&b>=0;j+=2,b-=2) {
+                    ans[j] = '1';
+                    if(n % 2) ans[j + 1] = '1';
+                }
+                break;
+            }
+        }
+        cout << ans << endl;
+    }
+}
