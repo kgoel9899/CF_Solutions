8aacfbe61bc9cdf280a304ca14b8aa6637856ac1
Kshitij Goel <kshitijg9899@gmail.com>
2023-09-14 00:00:00 +0530

    Add solution for problem `A - Wizard of Orz` Link: https://codeforces.com/contest/1467/problem/A Tags: constructive algorithms, greedy, math, *900 Ref: https://codeforces.com/contest/1467/submission/223262946

--- codeforces/1467/A.cpp
+++ codeforces/1467/A.cpp
@@ -1,44 +1,30 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
 #define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
+int solveTestCase() {
+    int n;
+    cin >> n;
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+    string s = "989";
+    if (n <= 3)
+        return cout << s.substr(0, n) << "\n", 0;
 
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+    cout << s;
+    for (int i = 3; i < n; i++)
+        cout << (i - 3) % 10;
+    cout << "\n";
+    return 0;
+}
 
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n;
-        cin >> n;
-        if(n == 1) cout << 9 << endl;
-        else if(n == 2) cout << 98 << endl;
-        else {
-            cout << 989;
-            int x = -1;
-            for(int i=3;i<n;i++) {
-                x++;
-                x %= 10;
-                cout << x;
-            }
-            cout << endl;
-        }
-    }
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+        solveTestCase();
+
 }
