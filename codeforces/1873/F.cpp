c9d56603093b53cfdf64c655b11fdbc7909d6436
Kshitij Goel <kshitijg9899@gmail.com>
2023-10-13 23:53:00 +0530

    Add solution for problem `F - Money Trees` Link: https://codeforces.com/contest/1873/problem/F Tags: binary search, greedy, math, two pointers, *1300 Ref: https://codeforces.com/contest/1873/submission/228074324

--- codeforces/1873/F.cpp
+++ codeforces/1873/F.cpp
@@ -1,63 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
-
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
-#else
-#define dbg(...)
-#endif
-
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> a(n), h(n);
-        for(int i=0;i<n;i++) {
-            cin >> a[i];
+ 
+const int N = 200'000;
+ 
+int n, k;
+int a[N+5], h[N+5], pref[N+5], length[N+5];
+ 
+bool get(int dist)
+{
+    bool found = false;
+    for(int i = 0; i < n-dist+1; i++)
+    {
+        if(length[i] < dist){continue;}
+        int sum = pref[i+dist]-pref[i];
+        if(sum <= k)
+        {
+            found = true;
+            break;
         }
-        for(int i=0;i<n;i++) {
-            cin >> h[i];
+    }
+    return found;
+}
+ 
+void solve()
+{
+    pref[0] = 0;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        pref[i+1] = pref[i]+a[i];
+    }
+    for(int i = 0; i < n; i++)
+    {
+        cin >> h[i];
+    }
+    length[n-1] = 1;
+    for(int i = n-2; i >= 0; i--)
+    {
+        if(h[i]%h[i+1] == 0)
+        {
+            length[i] = length[i+1]+1;
         }
-        vector<int> v(n, 1);
-        for(int i=n-2;i>=0;i--) {
-            if(h[i] % h[i + 1] == 0) v[i] = v[i + 1] + 1;
+        else
+        {
+            length[i] = 1;
         }
-        vector<int> pref(n);
-        pref[0] = a[0];
-        for(int i=1;i<n;i++) {
-            pref[i] = pref[i - 1] + a[i];
+    }
+    int l = 1, r = N;
+    while(l <= r)
+    {
+        int mid = (l+r)/2;
+        if(get(mid))
+        {
+            l = mid+1;
         }
-        int ans = 0;
-        for(int i=0;i<n;i++) {
-            int beg = i, end = i + v[i] - 1;
-            while(beg <= end) {
-                int mid = (beg + end) / 2;
-                int curr = pref[mid] - (i ? pref[i - 1] : 0);
-                if(curr > k) end = mid - 1;
-                else {
-                    // dbg(i, mid, curr);
-                    ans = max(ans, mid - i + 1);
-                    beg = mid + 1;
-                }
-            }
+        else
+        {
+            r = mid-1;
         }
-        cout << ans << endl;
+    }
+    cout << r << endl;
+}
+ 
+int main() {
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
     }
 }
