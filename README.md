c8f9933b3daf1106321c078dfa66fd0608c0d398
Kshitij Goel <kshitijg9899@gmail.com>
2023-10-18 00:39:00 +0530

    Add solution for problem `C - Make it Alternating` Link: https://codeforces.com/contest/1879/problem/C Tags: combinatorics, dp, greedy, *1300 Ref: https://codeforces.com/contest/1879/submission/228597770

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ A repository to keep track of problem solving practice, containing solutions fro
 
 | # | Title | Solution | Tags | Submitted |
 |---| ----- | -------- | ---- | --------- |
-1626 | [C - Make it Alternating](https://codeforces.com/contest/1879/problem/C) | [GNU C++20 (64)](./codeforces/1879/C.cpp) | `combinatorics` `dp` `greedy` `*1300` | Oct/17/2023 23:59 | 
+1626 | [C - Make it Alternating](https://codeforces.com/contest/1879/problem/C) | [GNU C++20 (64)](./codeforces/1879/C.cpp) | `combinatorics` `dp` `greedy` `*1300` | Oct/18/2023 00:39 | 
 1625 | [A - Hongcow Builds A Nation](https://codeforces.com/contest/744/problem/A) | [GNU C++20 (64)](./codeforces/744/A.cpp) | `dfs and similar` `graphs` `*1500` | Oct/17/2023 20:11 | 
 1624 | [C - Hongcow Builds A Nation](https://codeforces.com/contest/745/problem/C) | [GNU C++20 (64)](./codeforces/745/C.cpp) | `constructive algorithms` `dfs and similar` `graphs` `*1500` | Oct/17/2023 20:10 | 
 1623 | [B - Shoe Shuffling](https://codeforces.com/contest/1691/problem/B) | [GNU C++20 (64)](./codeforces/1691/B.cpp) | `constructive algorithms` `greedy` `implementation` `two pointers` `*1000` | Oct/16/2023 21:41 | 
--- codeforces/1879/C.cpp
+++ codeforces/1879/C.cpp
@@ -1,35 +1,61 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
 
-const int MOD = 998'244'353;
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
 
-void upd(int &a, int b) {
-    a = (a * 1LL * b) % MOD;
-}
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
 
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
 
-int t;
-string s;
-
-int main() {
-	cin >> t;
-	for (int tc = 0; tc < t; ++tc) {
-	    cin >> s;
-	    int res = 1;
-	    int k = s.size();
-	    int n = s.size();
-	    for (int l = 0; l < n; ) {
-	        int r = l + 1;
-	        while(r < n && s[l] == s[r])
-	            ++r;
-            upd(res, r - l);
-            --k;
-            l = r;
-	    }
-	    
-	    for (int i = 1; i <= k; ++i)
-	        upd(res, i);
-        cout << k << ' ' << res << endl;
-	}
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    const int N = 2e5 + 5;
+    vector<int> fact(N, 1);
+    for(int i=2;i<N;i++) {
+        fact[i] = fact[i - 1] * i;
+        fact[i] %= mod;
+    }
+    while(tt--) {
+        string s;
+        cin >> s;
+        int n = s.size();
+        char curr = s[0];
+        int ct = 1, ans = 0, ways = 1;
+        for(int i=1;i<n;i++) {
+            if(s[i] == curr) ct++;
+            else {
+                if(ct > 1) {
+                    ans += ct - 1;
+                    ways *= ct;
+                    ways %= mod;
+                }
+                ct = 1;
+                curr = s[i];
+            }
+        }
+        if(ct > 1) {
+            ans += ct - 1;
+            ways *= ct;
+            ways %= mod;
+        }
+        // dbg(ways);
+        // dbg(groups);
+        cout << ans << " " << (ways * fact[ans]) % mod << endl;
+    }
 }
