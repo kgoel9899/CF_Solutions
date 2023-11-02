006169eb70a3053f4995e20036e836d0843d3165
Kshitij Goel <kshitijg9899@gmail.com>
2023-11-02 23:46:00 +0530

    Add solution for problem `A - K-divisible Sum` Link: https://codeforces.com/contest/1476/problem/A Tags: binary search, constructive algorithms, greedy, math, *1000 Ref: https://codeforces.com/contest/1476/submission/231022590

--- codeforces/1476/A.cpp
+++ codeforces/1476/A.cpp
@@ -29,9 +29,13 @@ int32_t main() {
     while(tt--) {
         int n, k;
         cin >> n >> k;
-        if(n >= k) cout << 1 + (n % k != 0) << endl;
-        else {
-            cout << (k + n - 1) / n << endl;
+        if(n >= k) {
+            // if(n % k == 0) cout << 1 << endl;
+            // else cout << 2 << endl;
+            cout << 1 + (n % k != 0) << endl;
+        } else {
+            if(k % n == 0) cout << k / n << endl;
+            else cout << (k + n - 1) / n << endl;
         }
         xx++;
     }
