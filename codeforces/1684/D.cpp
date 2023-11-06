ef634c26c1542e034041373e91f940f8cf79fa48
Kshitij Goel <kshitijg9899@gmail.com>
2023-11-06 23:55:00 +0530

    Add solution for problem `D - Traps` Link: https://codeforces.com/contest/1684/problem/D Tags: constructive algorithms, greedy, sortings, *1700 Ref: https://codeforces.com/contest/1684/submission/231631528

--- codeforces/1684/D.cpp
+++ codeforces/1684/D.cpp
@@ -1,47 +1,49 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+// https://codeforces.com/contest/1684/submission/157681158
+/**
+ *    author:  tourist
+ *    created: 19.05.2022 18:46:17       
+**/
+#include <bits/stdc++.h>
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
 #else
-#define dbg(...)
+#define debug(...) 42
 #endif
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-// https://www.youtube.com/watch?v=-mHfkL5xn1w&ab_channel=CompetitiveCoding-NewtonSchool
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        int n, k;
-        cin >> n >> k;
-        vector<int> v(n);
-        int ans = 0;
-        for(int i=0;i<n;i++) {
-            cin >> v[i];
-            ans += v[i];
-            v[i] -= (n - 1 - i);
-        }
-        sort(all(v));
-        reverse(all(v));
-        for(int i=0;i<k;i++) {
-            ans -= v[i];
-        }
-        ans -= k * (k - 1) / 2;
-        cout << ans << endl;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
     }
+    vector<int> order(n);
+    iota(order.begin(), order.end(), 0);
+    sort(order.begin(), order.end(), [&](int i, int j) {
+      return a[i] + i > a[j] + j;
+    });
+    vector<bool> skip(n, false);
+    for (int i = 0; i < k; i++) {
+      skip[order[i]] = true;
+    }
+    int bonus = 0;
+    long long ans = 0;
+    for (int i = 0; i < n; i++) {
+      if (skip[i]) {
+        bonus += 1;
+      } else {
+        ans += a[i] + bonus;
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
 }
