8f1462eeb7406259a18b657c188ca0c584ca7502
Kshitij Goel <kshitijg9899@gmail.com>
2024-01-09 23:49:00 +0530

    Add solution for problem `B - Swap and Delete` Link: https://codeforces.com/contest/1913/problem/B Tags: strings, *1000 Ref: https://codeforces.com/contest/1913/submission/240975337

--- codeforces/1913/B.cpp
+++ codeforces/1913/B.cpp
@@ -1,46 +1,41 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define MOD 1000000007
-#define mod 998244353
-#define int long long
-#define setpres cout << fixed << setprecision(10)
-#define all(x) (x).begin(), (x).end()
-#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define endl "\n"
-const int INF = 1e18;
+// https://codeforces.com/contest/1913/submission/237734023
+// CHECK
+/**
+ *    author:  tourist
+ *    created: 18.12.2023 09:37:30       
+**/
+#include <bits/stdc++.h>
 
-#ifdef DEBUG
-#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+using namespace std;
+
+#ifdef LOCAL
+#include "algo/debug.h"
 #else
-#define dbg(...)
+#define debug(...) 42
 #endif
 
-template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
-template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
-
-void dbg_out() { cout << endl; }
-template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
-
-int32_t main() {
-    fast;
-    int tt = 1;
-    cin >> tt;
-    while(tt--) {
-        string s;
-        cin >> s;
-        int n = s.size();
-        int o = 0, z = 0;
-        for(auto& i : s) {
-            if(i == '1') o++;
-            else z++;
-        }
-        int i = 0;
-        while(i < n) {
-            if(s[i] == '1' && z) z--;
-            else if(s[i] == '0' && o) o--;
-            else break;
-            i++;
-        }
-        cout << n - i << endl;
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    string s;
+    cin >> s;
+    int n = (int) s.size();
+    vector<int> cnt(2);
+    for (char c : s) {
+      cnt[(int) (c - '0')] += 1;
     }
+    vector<int> need(2);
+    int ans = n;
+    for (int i = 0; i < n; i++) {
+      need[(int) (s[i] - '0') ^ 1] += 1;
+      if (cnt[0] >= need[0] && cnt[1] >= need[1]) {
+        ans = n - 1 - i;
+      }
+    }
+    cout << ans << '\n';
+  }
+  return 0;
 }
