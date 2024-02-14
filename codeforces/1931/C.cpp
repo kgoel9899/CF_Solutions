1419171d9f982b2e850c581fd9358cfb1587954d
Kshitij Goel <kshitijg9899@gmail.com>
2024-02-14 12:47:00 +0530

    Add solution for problem `C - Make Equal Again` Link: https://codeforces.com/contest/1931/problem/C Tags: brute force, greedy, math, *1000 Ref: https://codeforces.com/contest/1931/submission/246324838

---
+++ codeforces/1931/C.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define mod 998244353
+#define int long long
+#define setpres cout << fixed << setprecision(10)
+#define all(x) (x).begin(), (x).end()
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+const int INF = 1e18;
+
+#ifdef DEBUG
+#define dbg(...) cout << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cout << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cout << ' ' << H; dbg_out(T...); }
+
+int solve(int val, int n, vector<int>& v) {
+    int i = 0, j = n - 1;
+    while(i < n && v[i] == val) {
+        i++;
+    }
+    while(j >= 0 && v[j] == val) {
+        j--;
+    }
+    return max(j - i + 1, 0ll);
+}
+int32_t main() {
+    fast;
+    int tt = 1;
+    cin >> tt;
+    while(tt--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++) {
+            cin >> v[i];
+        }
+        cout << min(solve(v[0], n, v), solve(v[n - 1], n, v)) << endl;
+    }
+}
